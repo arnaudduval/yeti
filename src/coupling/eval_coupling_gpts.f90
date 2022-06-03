4164d186d3bdb393fa2f36ccb6a6658f79382f26
Arnaud Duval <arnaud.duval@insa-lyon.fr>
2022-06-03 14:19:14 +0200

    Prepare environment to create a routrine that compute points on the surfaces involved in U5 coupling.

---
+++ src/coupling/eval_coupling_gpts.f90
@@ -0,0 +1,15 @@
+!! Compute points on the surfaces involved in U5 coupling
+
+
+subroutine eval_coupling_gpts()
+    use iso_fortran_env
+
+    use parameters
+    use nurbspatch
+    use embeddedMapping
+
+    implicit none
+
+
+
+end subroutine eval_coupling_gpts
