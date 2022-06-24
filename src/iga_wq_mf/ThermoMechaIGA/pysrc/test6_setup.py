2f69b6d519051e3a1039994c963f151166b905bb
JoaquinCornejo <joaquin.cornejo-fuentes@insa-lyon.fr>
2022-06-24 10:49:57 +0200

    Add gaus legendre points in fortran and python

--- src/iga_wq_mf/ThermoMechaIGA/pysrc/test6_setup.py
+++ src/iga_wq_mf/ThermoMechaIGA/pysrc/test6_setup.py
@@ -7,12 +7,12 @@
 # Python libraries
 import os, time
 import numpy as np
-from scipy import sparse as sp
 import pandas as pd
 import matplotlib.pyplot as plt
+from mpltools import annotation
 
 # My libraries
-from lib.fortran_mf_wq import fortran_mf_wq, wq_find_basis_weights_fortran
+from lib.fortran_mf_wq import wq_find_basis_weights_fortran
 from iga_wq_mf import assembly
 
 # Choose folder
@@ -21,17 +21,17 @@ folder = os.path.dirname(full_path) + '/results/test6/'
 if not os.path.isdir(folder): os.mkdir(folder)
 
 dataExist = True
-nbel = 64
+nbel = 40
 
 # Set filename
 filename_WQ = 'setup_time_WQ_'  + 'nbel_' + str(nbel) 
 filename_WQ = folder + filename_WQ
 
 # Define inputs
-degree_list = np.arange(2, 7)
+degree_list = np.arange(9, 11)
 
 # Output
-time_matrix = np.zeros((len(degree_list), 5))
+time_matrix = np.zeros((len(degree_list), 3))
 time_matrix[:, 0] = degree_list
 
 if not dataExist:
@@ -42,44 +42,42 @@ if not dataExist:
 
         # Some other inputs
         nb_rows = nb_cols = (degree+nbel)**3
+        
+        # ------------
         capacity_coefs = np.ones(len(qp_wq)**3)
-        conductivity_coefs = np.ones((3, 3, len(qp_wq)**3))
-
-        # Initialize
-        shape_matrices, indices, data_C, data_K, size_I = [], [], [], [], []
+        shape_matrices, indices, data_C, size_I = [], [], [], []
         for dim in range(3):
             shape_matrices.append(len(qp_wq))
             indices.append(indi); indices.append(indj) 
             data_C.append(dB0); data_C.append(dW00)
-            data_K.append(dB0); data_K.append(dB1)
-            data_K.append(dW00); data_K.append(dW01)
-            data_K.append(dW10); data_K.append(dW11)
             size_I.append(nnz_I)
 
         inputs_C = [capacity_coefs, *shape_matrices, *indices, *data_C, *size_I]
-        inputs_K = [conductivity_coefs, *shape_matrices, *indices, *data_K, *size_I]
         
         start = time.time()
-        val_C, indi_C, indj_C = assembly.wq_get_capacity_3d(*inputs_C)
-        stop1 = time.time()
-        CC = sp.csr_matrix((val_C, indj_C, indi_C), shape=(nb_rows, nb_cols))
-        stop2 = time.time()
-        time_matrix[i, 1] = stop1 - start 
-        time_matrix[i, 2] = stop2 - start 
+        assembly.wq_get_capacity_3d(*inputs_C)
+        stop = time.time()
+        time_matrix[i, 1] = stop - start 
+        del inputs_C, capacity_coefs
+
+        # ------------
+        conductivity_coefs = np.ones((3, 3, len(qp_wq)**3))
+        data_K = []
+        for dim in range(3):
+            data_K.append(dW00); data_K.append(dW01)
+            data_K.append(dW10); data_K.append(dW11)
+            data_K.append(dB0); data_K.append(dB1)
+
+        inputs_K = [conductivity_coefs, *shape_matrices, *indices, *data_K, *size_I]
 
         start = time.time()
-        val_K, indi_K, indj_K = assembly.wq_get_conductivity_3d(*inputs_K)
-        stop1 = time.time()
-        KK = sp.csr_matrix((val_K, indj_K, indi_K), shape=(nb_rows, nb_cols))
-        stop2 = time.time()
+        assembly.wq_get_conductivity_3d(*inputs_K)
         stop = time.time()
-        time_matrix[i, 3] = stop1 - start 
-        time_matrix[i, 4] = stop2 - start 
-
-        del CC, KK
+        time_matrix[i, 2] = stop - start 
+        del inputs_K, conductivity_coefs
 
         print(time_matrix[i, :])
-        np.savetxt(filename_WQ, time_matrix)
+        # np.savetxt(filename_WQ, time_matrix)
 
 else :
 
@@ -88,10 +86,17 @@ else :
         setup_WQ = np.loadtxt(filename_WQ)
         litterature = pd.read_table(folder + 'lit_WQ.dat', sep='\t', names=['degree', 'time'])
 
-        # plot
+        # Plot
         fig, ax1 = plt.subplots(nrows=1, ncols=1, figsize=(6,4))
         ax1.loglog(litterature.degree, litterature.time, 'o--', label='Litterature')
         ax1.loglog(setup_WQ[:,0], setup_WQ[:,1], 'x--', label='My algo')
+
+        # Slope
+        slope, _ = np.polyfit(np.log10(setup_WQ[:,0]),np.log10(setup_WQ[:,1]), 1)
+        slope = round(slope, 3)
+        annotation.slope_marker((setup_WQ[2,0], setup_WQ[2,1]), slope, 
+                                text_kwargs={'fontsize': 14},
+                                poly_kwargs={'facecolor': (0.73, 0.8, 1)})
 
         # Plot reference
         c1, c2 = 0.125, 0.125
@@ -118,12 +123,14 @@ else :
         setup_WQ = np.loadtxt(filename_WQ)
         litterature = pd.read_table(folder + 'lit_MF.dat', sep='\t', names=['degree', 'C', 'K'])
 
-        # plot
+        # Plot
         fig, [ax1, ax2] = plt.subplots(nrows=1, ncols=2, figsize=(12,4))
         ax1.loglog(litterature.degree, litterature.C, 'o--', label='Litterature')
         ax1.loglog(setup_WQ[:,0], setup_WQ[:,1], 'x--', label='My algo')
+
         ax2.loglog(litterature.degree, litterature.K, 'o--', label='Litterature')
-        ax2.loglog(setup_WQ[:,0], setup_WQ[:,3], 'x--', label='My algo')
+        ax2.loglog(setup_WQ[:,0], setup_WQ[:,2], 'x--', label='My algo')
+
 
         # Plot reference
         c1, c2 = 0.125, 0.125
