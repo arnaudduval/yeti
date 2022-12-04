6f5c6738f63ecfa4038bf077a4f572af953c5a3f
Arnaud Duval <arnaud.duval@insa-lyon.fr>
2022-12-04 23:08:23 +0100

    Improve code style of bench OPTplatevib_semi_AN

--- benchs/platevib/OPTplatevib_semi_AN.py
+++ benchs/platevib/OPTplatevib_semi_AN.py
@@ -2,17 +2,19 @@
 
 # This file is part of Yeti.
 #
-# Yeti is free software: you can redistribute it and/or modify it under the terms 
-# of the GNU Lesser General Public License as published by the Free Software 
-# Foundation, either version 3 of the License, or (at your option) any later version.
+# Yeti is free software: you can redistribute it and/or modify it under the
+# terms of the GNU Lesser General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option)
+# any later version.
 #
-# Yeti is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
-# without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
-# PURPOSE. See the GNU Lesser General Public License for more details.
+# Yeti is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+# details.
 #
-# You should have received a copy of the GNU Lesser General Public License along 
-# with Yeti. If not, see <https://www.gnu.org/licenses/>
-# 
+# You should have received a copy of the GNU Lesser General Public License
+# along with Yeti. If not, see <https://www.gnu.org/licenses/>
+#
 # #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
@@ -23,160 +25,172 @@ validation is performed to verify is target frequencies are obtained
 """
 
 # Python module
-import numpy as np
 import sys
-import time
+import numpy as np
 
-#IGA module
-from preprocessing.igaparametrization import IGAparametrization, IGAmanip as manip
-import postprocessing.postproc as pp
+# IGA module
 import reconstructionSOL as rsol
+from preprocessing.igaparametrization import IGAparametrization,    \
+                                             IGAmanip as manip,     \
+                                             OPTmodelling
+import postprocessing.postproc as pp
 
-from preprocessing.igaparametrization import OPTmodelling
+if __name__ == "__main__":
+    # Base path for .inp and .NB files
+    FILENAME = 'plateVolume'
 
+    # Creation of the IGA object
+    modeleIGA = IGAparametrization(filename=FILENAME)
 
-# Base path for .inp and .NB files
-FILENAME = 'plateVolume'
+    # Refine IGA object along 1st and 2nd directions
+    nb_deg = np.array([1, 1, 0])
+    nb_ref = np.array([2, 2, 0])
+    modeleIGA.refine(nb_ref, nb_deg)
 
-# Creation of the IGA object
-modeleIGA = IGAparametrization(filename=FILENAME)
+    # Parametrization
+    # Get index of CP at top and bottom
+    botcps = manip.get_directionCP(modeleIGA, 5, 0, 0) - 1
+    topcps = manip.get_directionCP(modeleIGA, 6 ,0, 0) - 1
 
-# Refine IGA object along 1st and 2nd directions
-nb_deg = np.array([1,1,0])
-nb_ref = np.array([2,2,0])
-modeleIGA.refine(nb_ref,nb_deg)
+    nb_var = botcps.size
 
+    # Define thickness map depending on design variables
+    H_MAX = 0.5
+    H_MIN = 0.05
 
-# Parametrization
-# Get index of CP at top and bottom
-botcps = manip.get_directionCP(modeleIGA, 5,0,0) - 1
-topcps = manip.get_directionCP(modeleIGA, 6,0,0) - 1
+    def thickness(coords0, igapara, var):
+        """
+        Shape parametrization.
+        Move control points as a function of esign variables corresponding to
+        thickness
+        """
+        var = (H_MAX - H_MIN)*var + H_MIN
+        
+        igapara.coords[:, :] = coords0[:, :]
+        igapara.coords[2, botcps] = -0.5 * ((H_MAX - H_MIN)*var + H_MIN)
+        igapara.coords[2, topcps] = 0.5 * ((H_MAX-H_MIN)*var + H_MIN)
 
-nb_var = botcps.size
+    # Refinement from optimization model to analysis model
+    nb_deg = np.array([0, 0, 1])
+    nb_ref = np.array([2, 2, 0])
 
-# Define thickness map depending on design variables
-hM = 0.5
-hm = 0.05
-def thickness(coords0,igapara,var):
-    
-    var = (hM-hm)*var + hm
-    
-    igapara._COORDS[:,:]      = coords0[:,:]
-    igapara._COORDS[2,botcps] =-0.5 * ( (hM-hm)*var + hm )
-    igapara._COORDS[2,topcps] = 0.5 * ( (hM-hm)*var + hm )
-    return None
+    # Define optim problem
+    # modeleIGA -> initial parametrization
+    # nb_var    -> number of design variables
+    # thickness -> function updating coordinates of the coarse (optim) model
+    # nb_degreeElevationByDirection -> degree elevation for each diretion
+    # nb_refinementByDirection -> refinement in each direction
+    optPB = OPTmodelling(modeleIGA, nb_var, thickness,
+                         nb_degreeElevationByDirection=nb_deg,
+                         nb_refinementByDirection=nb_ref)
 
+    # OPTIMIZATION
 
-# Refinement from optimization model to analysis model
-nb_deg = np.array([0,0,1])
-nb_ref = np.array([2,2,0])
+    from scipy.optimize import minimize
 
-# Define optim problem
-# modeleIGA -> initial parametrization
-# nb_var    -> number of design variables
-# thickness -> function updating coordinates of the coarse (optim) model
-# nb_degreeElevationByDirection -> degree elevation for each diretion
-# nb_refinementByDirection -> refinement in each direction
-optPB = OPTmodelling(modeleIGA, nb_var, thickness,
-                     nb_degreeElevationByDirection = nb_deg, 
-                     nb_refinementByDirection      = nb_ref)
+    # Initial values of design variables
+    x0 = 0.6*np.ones(nb_var)
+    # Compute initial 1st eigenvalue (m0) and eigenvector (v0)
+    m0, v0 = optPB.compute_vibrationMode(x0)
 
+    # Define target eigenvalue(s)
+    NB_FRQCIBLE = 2
+    xcible = 0.2*np.ones(nb_var)
+    wcible, vcible = optPB.compute_vibrationMode(xcible, nb_frq=NB_FRQCIBLE)
 
-# OPTIMIZATION
-
-from scipy.optimize import minimize
-
-# Initial values of design variables
-x0    = 0.6*np.ones(nb_var)
-# Compute initial 1st eigenvalue (m0) and eigenvector (v0)
-m0,v0 = optPB.compute_vibrationMode(x0)
-
-# Define target eigenvalue(s)
-nb_frqCible = 2
-xcible      = 0.2*np.ones(nb_var)
-wcible,vcible = optPB.compute_vibrationMode(xcible,nb_frq=nb_frqCible)
-
-# Define a function ans its gradient w.r.t design variables
-# Gap with target eigenvalues
-def frecIGA(xk):
-    valsi,vecti = optPB.compute_vibrationMode(xk,nb_frq=nb_frqCible)
-    mi = np.sum((valsi - wcible)**2.)
-    return mi / np.sum(wcible**2.)
-
-def gradFrecIGA(xk):
-    gradF = np.zeros_like(xk)
-    valsi,vecti = optPB.compute_vibrationMode(xk, nb_frq=nb_frqCible)
-    gradFreq = optPB.compute_gradVibration_semiAN(xk,eps = 1.e-6,nb_frq=nb_frqCible)
-    for i in np.arange(nb_frqCible):
-        gradF[:] += 2.*gradFreq[:,i]*(valsi[i] - wcible[i])
-    return gradF  / np.sum(wcible**2.)
-
-# Define a function and its gradient w.r.t design variables
-# Volume relative to initial volume
-V0 = optPB.compute_volume(x0)
-def volIGA(xk):
-    global V0
-    return optPB.compute_volume(xk)/V0
-def gradVolIGA(xk):
-    global V0
-    return optPB.compute_gradVolume_AN(xk)/V0
+    # Define a function ans its gradient w.r.t design variables
+    # Gap with target eigenvalues
+    def frecIGA(x_k):
+        """
+        Cost function
+        Return relative difference between eigenfrequencies and reference
+        values
+        """
+        valsi, _ = optPB.compute_vibrationMode(x_k, nb_frq=NB_FRQCIBLE)
+        m_i = np.sum((valsi - wcible)**2.)
+        return m_i / np.sum(wcible**2.)
 
 
+    def gradFrecIGA(xk):
+        """
+        Gradient of cost function
+        """
+        gradF = np.zeros_like(xk)
+        valsi, _ = optPB.compute_vibrationMode(xk, nb_frq=NB_FRQCIBLE)
+        gradFreq = optPB.compute_gradVibration_semiAN(xk, eps=1.e-6,
+                                                      nb_frq=NB_FRQCIBLE)
+        for i in np.arange(NB_FRQCIBLE):
+            gradF[:] += 2.*gradFreq[:, i]*(valsi[i] - wcible[i])
+        return gradF / np.sum(wcible**2.)
+
+    # Define a function and its gradient w.r.t design variables
+    # Volume relative to initial volume
+    vol0 = optPB.compute_volume(x0)
+
+    def volIGA(x_k):
+        """
+        Constraint function : volume relative to initial volume
+        """
+        global vol0
+        return optPB.compute_volume(x_k)/vol0
 
 
-tab = []
-iopt= 0 
+    def gradVolIGA(x_k):
+        """
+        Gradient of constraint function
+        """
+        global vol0
+        return optPB.compute_gradVolume_AN(x_k)/vol0
 
-# Define callback function to be run at each optimization iteration (iopt)
-def saveXk(xk):
-    global iopt
-    print(('\nIteration%i'%iopt))
-    valsi,vecti = optPB.compute_vibrationMode(xk)
-    tab.append([xk[0],valsi[0]])
-    
-    # - Plot Thickness
-    ticknessfield = np.zeros_like(optPB._coarseParametrization._COORDS)
-    ticknessfield[2,:] = np.tile(  optPB._coarseParametrization._COORDS[2,topcps]
-                                   - optPB._coarseParametrization._COORDS[2,botcps], 
-                                   (2,1)).flatten('C')
-    pp.generatevtu(*optPB._coarseParametrization.get_inputs4postprocVTU(
-        'vibOpt%0.2d'%iopt,ticknessfield,
-        nb_ref=np.array([4,4,1]),Flag=np.array([True, False, False])))
-    np.savetxt('results/cps%0.2d.txt'%iopt,optPB._coarseParametrization._COORDS.T,delimiter=',')
+    tab = []
+    iopt = 0
 
-    # - Plot Analysis
-    
-    SOL,u = rsol.reconstruction(
-        **optPB._fineParametrization.get_inputs4solution(vecti))
-    pp.generatevtu(*optPB._fineParametrization.get_inputs4postprocVTU(
-        'vibAn%0.2d'%iopt,  SOL.transpose(),
-        nb_ref=np.array([2,2,2]), Flag=np.array([True, False, False])))
-    
-    iopt += 1
-    
-    return None
+    # Define callback function to be run at each optimization iteration (iopt)
+    def saveXk(x_k):
+        """
+        Callback function.
+        Save results at each optimization iteration
+        """
+        global iopt
+        print(f'\nIteration{iopt:02}')
+        valsi, vecti = optPB.compute_vibrationMode(x_k)
+        tab.append([x_k[0], valsi[0]])
 
-# Bounds for design variables : [0 ; 1]
-bds = ((0.,1.),)*nb_var
+        # - Plot Thickness
+        ticknessfield = np.zeros_like(optPB._coarseParametrization.coords)
+        ticknessfield[2, :] = np.tile(
+            optPB._coarseParametrization.coords[2, topcps]
+            - optPB._coarseParametrization.coords[2, botcps],
+            (2, 1)).flatten('C')
+        pp.generatevtu(*optPB._coarseParametrization.get_inputs4postprocVTU(
+            f'vibOpt{iopt:02}', ticknessfield,
+            nb_ref=np.array([4, 4, 1]), Flag=np.array([True, False, False])))
+        np.savetxt(f'results/cps{iopt:02}.txt',
+                   optPB._coarseParametrization.coords.T,
+                   delimiter=',')
 
-# Run optimization
+        # - Plot Analysis
 
-## 1 - get given 1st eigenvalue
-res = minimize(frecIGA,x0,method='SLSQP',jac=gradFrecIGA,bounds=bds,callback=saveXk, tol = 1.e-9)
+        sol, _ = rsol.reconstruction(
+            **optPB._fineParametrization.get_inputs4solution(vecti))
+        pp.generatevtu(*optPB._fineParametrization.get_inputs4postprocVTU(
+            f'vibAn%0.2d{iopt:02}',  sol.transpose(),
+            nb_ref=np.array([2, 2, 2]), Flag=np.array([True, False, False])))
 
-if not res['success'] or res['fun'] > 0.02:
-    print("res['success'] : ", res['success'])
-    print("res['fun'] : ", res['fun'])
-    sys.exit(-1)
-else:
-    sys.exit(0)
+        iopt += 1
 
+    # Bounds for design variables : [0 ; 1]
+    bds = ((0., 1.),)*nb_var
 
+    # Run optimization
 
+    # 1 - get given 1st eigenvalue
+    res = minimize(frecIGA, x0, method='SLSQP', jac=gradFrecIGA,
+                   bounds=bds, callback=saveXk, tol=1.e-9)
 
-## 2 - minimize volume with a a given 1st eigenvalue
-#constraint_frq = ({'type': 'eq', 'fun':frecIGA, 'jac':gradFrecIGA})
-#res = minimize(volIGA,x0,method='SLSQP',jac=gradVolIGA,bounds=bds,constraints=constraint_frq,
-#               callback=saveXk,tol=1.e-4)
-
-
+    if not res['success'] or res['fun'] > 0.02:
+        print("res['success'] : ", res['success'])
+        print("res['fun'] : ", res['fun'])
+        sys.exit(-1)
+    else:
+        sys.exit(0)
