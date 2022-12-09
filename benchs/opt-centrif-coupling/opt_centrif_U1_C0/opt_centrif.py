d8df8182aebfc94911e3dca8044923fbbfb8fc2a
DUVAL Arnaud <arnaud.duval@insa-lyon.fr>
2022-12-09 16:25:08 +0100

    Improve code style of opt-centrif bench

--- benchs/opt-centrif-coupling/opt_centrif_U1_C0/opt_centrif.py
+++ benchs/opt-centrif-coupling/opt_centrif_U1_C0/opt_centrif.py
@@ -26,19 +26,17 @@ Results (design variables values) are compared with reference numerical results.
 import numpy as np
 
 # yeti modules
-from preprocessing.igaparametrization import IGAparametrization
+from preprocessing.igaparametrization import IGAparametrization, OPTmodelling
 import postprocessing.postproc as pp
 import reconstructionSOL as rsol
-
-from preprocessing.igaparametrization import OPTmodelling
 
 if __name__ == "__main__":
 
     modeleIGA = IGAparametrization(filename='centrif_U1_C0')
 
     # Refinement to create optimisation model
-    nb_deg = np.zeros((3, modeleIGA._nb_patch),dtype=np.intp)
-    nb_ref = np.zeros((3, modeleIGA._nb_patch),dtype=np.intp)
+    nb_deg = np.zeros((3, modeleIGA.nb_patch), dtype=np.intp)
+    nb_ref = np.zeros((3, modeleIGA.nb_patch), dtype=np.intp)
 
     nb_ref[:, 0] = np.array([0, 2, 0])
     nb_deg[:, 0] = np.array([0, 1, 0])
@@ -52,30 +50,32 @@ if __name__ == "__main__":
     # Initial refinement (none)
     modeleIGA.refine(nb_ref, nb_deg, additional_knots)
 
-    icp = np.where(modeleIGA._COORDS[1, :] > 1.2)[0]
-    nb_var = np.unique(modeleIGA._COORDS[1, icp]).size
+    icp = np.where(modeleIGA.coords[1, :] > 1.2)[0]
+    nb_var = np.unique(modeleIGA.coords[1, icp]).size
 
     # Define shape change from design variables
     # min and max dimensions, assuming that design variables are in [0,1]
-    mindim = 0.1
-    maxdim = 3.5
+    MINDIM = 0.1
+    MAXDIM = 3.5
 
     def shapemodif(coords0, igapara, var):
-        igapara._COORDS[:,:] = coords0[:,:]
+        """"
+        Shape parametrization
+        Design variables drive coordinates of control point located at y > 1.2
+        """
+        igapara.coords[:, :] = coords0[:, :]
         # shape change is made on points with y coord higher than 3
-        icp = np.where(modeleIGA.coords[1, :] > 1.2)[0]
         i = 0
-        for y in np.unique(modeleIGA.coords[1, icp]):
+        for y_loc in np.unique(modeleIGA.coords[1, icp]):
             # WARNING exact real value comparison is unsafe
-            jcp = np.where(modeleIGA.coords[1, :] == y)[0]
+            jcp = np.where(modeleIGA.coords[1, :] == y_loc)[0]
 
-            igapara.coords[2, jcp[0]] = - (mindim + var[i]*(maxdim-mindim))/2.
-            igapara.coords[2, jcp[1]] = - (mindim + var[i]*(maxdim-mindim))/2.
-            igapara.coords[2, jcp[2]] = (mindim + var[i]*(maxdim-mindim))/2.
-            igapara.coords[2, jcp[3]] = (mindim + var[i]*(maxdim-mindim))/2.
+            igapara.coords[2, jcp[0]] = - (MINDIM + var[i]*(MAXDIM-MINDIM))/2.
+            igapara.coords[2, jcp[1]] = - (MINDIM + var[i]*(MAXDIM-MINDIM))/2.
+            igapara.coords[2, jcp[2]] = (MINDIM + var[i]*(MAXDIM-MINDIM))/2.
+            igapara.coords[2, jcp[3]] = (MINDIM + var[i]*(MAXDIM-MINDIM))/2.
 
             i += 1
-        return None
 
     # Refinement from optim model to analysis model
     nb_deg = np.array([1, 0, 1])
@@ -89,37 +89,48 @@ if __name__ == "__main__":
     from scipy.optimize import minimize
 
     # Initial values of design variables
-    x0 = ((1.5-mindim)/(maxdim-mindim))*np.ones(nb_var)
+    x0 = ((1.5-MINDIM)/(MAXDIM-MINDIM))*np.ones(nb_var)
     # Compute initial values
-    v0 = optPB.compute_volume(x0)
-    c0 = optPB.compute_compliance_discrete(x0)
+    V0 = optPB.compute_volume(x0)
+    C0 = optPB.compute_compliance_discrete(x0)
 
     # Define functions and gradients
-    def volIGA(xk):
-        global v0
-        return (optPB.compute_volume(xk)-v0)/v0
-    def gradVolIGA(xk):
-        global v0
-        return optPB.compute_gradVolume_AN(xk)/v0
+    def vol_iga(x_k):
+        """
+        Compute volume of the structure
+        """
+        return (optPB.compute_volume(x_k)-V0)/V0
 
-    def compIGA(xk):
-        global c0
-        return optPB.compute_compliance_discrete(xk)/c0
+    def grad_vol_iga(x_k):
+        """
+        Compute gradient of the volume with respect to the design variables
+        """
+        return optPB.compute_gradVolume_AN(x_k)/V0
 
-    def gradCompIGA(xk):
-        global c0
-        return optPB.compute_gradCompliance_AN(xk)/c0
+    def comp_iga(x_k):
+        """
+        Compute complianbce of the structure
+        """
+        return optPB.compute_compliance_discrete(x_k)/C0
+
+    def grad_comp_iga(x_k):
+        """
+        Compute gradient of the compliance with respect to the design variables
+        """
+        return optPB.compute_gradCompliance_AN(x_k)/C0
 
     iopt = 0
 
-    # Define callback function
-    def saveXk(xk):
+    def save_x_k(x_k):
+        """
+        Callback function saving results at each optimization iteration
+        """
         global iopt
-        print(('\nIteration%i'%iopt))
+        print((f'\nIteration {iopt:03}'))
         sol, _ = rsol.reconstruction(
                 **optPB._fineParametrization.get_inputs4solution(optPB._save_sol_fine))
         pp.generatevtu(*optPB._fineParametrization.get_inputs4postprocVTU(
-                        'OPT3-fine%0.2d'%iopt,  sol.transpose(),
+                        f'OPT3-fine{iopt:02}',  sol.transpose(),
                         nb_ref=3*np.array([1, 1, 1]),
                         Flag=np.array([True, False, False])))
         iopt += 1
@@ -127,12 +138,12 @@ if __name__ == "__main__":
     # Bounds for design variables
     bds = ((0., 1.),)*nb_var
 
-    constraint = ({'type': 'eq', 'fun': volIGA, 'jac': gradVolIGA})
-    x0 = ((1.5-mindim)/(maxdim-mindim))*np.ones(nb_var)
+    constraint = ({'type': 'eq', 'fun': vol_iga, 'jac': grad_vol_iga})
+    x0 = ((1.5-MINDIM)/(MAXDIM-MINDIM))*np.ones(nb_var)
 
-    res = minimize(compIGA, x0, method='SLSQP',
-                   jac=gradCompIGA, bounds=bds,
-                   constraints=constraint, callback=saveXk)
+    res = minimize(comp_iga, x0, method='SLSQP',
+                   jac=grad_comp_iga, bounds=bds,
+                   constraints=constraint, callback=save_x_k)
 
     # Verify results
     # Numerical reference result
