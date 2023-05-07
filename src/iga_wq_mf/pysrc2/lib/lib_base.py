1882cd1daaf02af5638f66327d3469ff0605bfd0
JoaquinCornejo <joaquin.cornejo-fuentes@insa-lyon.fr>
2023-05-07 09:49:19 +0200

    change file of solvers in python

--- src/iga_wq_mf/pysrc2/lib/lib_base.py
+++ src/iga_wq_mf/pysrc2/lib/lib_base.py
@@ -483,52 +483,111 @@ def computeMean3d(ncu, ncv, ncw, C):
 
 	return integral
 
-def CG(funcAu, b, nbIterPCG=100, threshold=1e-8):   
+class solver():
+	
+	def __init__(self):
+		self._nbIter       = 100
+		self._thresholdPCG = 1e-10
+		return
+	
+	def CG(self, Afun, b):
+		x = np.zeros(np.shape(b))
+		r = b; normb = np.max(np.abs(r))
+		resPCG = [1.0]
+		if normb <= self._thresholdPCG: return
+		rsold = np.dot(r, r); p = r
 
-	x = np.zeros(np.shape(b)); r = b
-	resPCG = np.zeros(nbIterPCG+1); resPCG[0] = 1.0
-	normb  = np.amax(np.absolute(r))
+		for i in range(self._nbIter):
+			Ap = Afun(p)
+			alpha = rsold/np.dot(p, Ap)
+			x += alpha*p
+			r -= alpha*Ap
 
-	if normb <= threshold: return
-	rsold = np.dot(r, r); p = r
+			resPCG.append(np.max(np.abs(r))/normb)
+			if (resPCG[-1]<=self._thresholdPCG): break
 
-	for iter in range(nbIterPCG):
-		Ap = funcAu(p)
-		alpha = rsold/np.dot(p, Ap)
-		x = x + alpha*p
-		r = r - alpha*Ap
+			rsnew = np.dot(r, r)
+			p = r + rsnew/rsold*p
+			rsold = rsnew
 
-		resPCG[iter+1] = np.amax(np.absolute(r))/normb
-		if resPCG[iter+1] <= threshold: break
-		
-		rsnew = np.dot(r, r)
-		p = r + rsnew/rsold * p
-		rsold = rsnew
+		return x, resPCG
+	
+	def PCG(self, Afun, Pfun, b):
+		x = np.zeros(np.shape(b))
+		r = b; normb = np.max(np.abs(r))
+		resPCG = [1.0]
+		if normb <= self._thresholdPCG: return
+		z = Pfun(r)
+		rsold = np.dot(r, z); p = z
 
-	return x, resPCG
+		for i in range(self._nbIter):
+			Ap = Afun(p)
+			alpha = rsold/np.dot(p, Ap)
+			x += alpha*p
+			r -= alpha*Ap
 
-def PCG(funcAu, funcPre, b, nbIterPCG=100, threshold=1e-8):   
+			resPCG.append(np.max(np.abs(r))/normb)
+			if (resPCG[-1]<=self._thresholdPCG): break
 
-	x = np.zeros(np.shape(b)); r = b
-	resPCG = np.zeros(nbIterPCG+1); resPCG[0] = 1.0
-	normb  = np.amax(np.absolute(r))
+			z = Pfun(r)
+			rsnew = np.dot(r, z)
+			p = z + rsnew/rsold*p
+			rsold = rsnew
 
-	if normb <= threshold: return
-	z = funcPre(r)
-	rsold = np.dot(r, r); p = z
+		return x, resPCG
+	
+	def BiCGSTAB(self, Afun, b):
+		x = np.zeros(np.shape(b))
+		r = b; normb = np.max(np.abs(r))
+		resPCG = [1.0]
+		if normb <= self._thresholdPCG: return
+		rhat = r; p = r
+		rsold = np.dot(r, rhat)
 
-	for iter in range(nbIterPCG):
-		Ap = funcAu(p)
-		alpha = rsold/np.dot(p, Ap)
-		x = x + alpha*p
-		r = r - alpha*Ap
+		for i in range(self._nbIter):
+			Ap = Afun(p)
+			alpha = rsold/np.dot(Ap, rhat)
+			s = r - alpha*Ap
+			As = Afun(s)
+			omega = np.dot(As, s)/np.dot(As, As)
+			x += alpha*p + omega*s
+			r = s - omega*As
 
-		resPCG[iter+1] = np.amax(np.absolute(r))/normb
-		if resPCG[iter+1] <= threshold: break
+			resPCG.append(np.max(np.abs(r))/normb)
+			if (resPCG[-1]<=self._thresholdPCG): break
 
-		z = funcPre(r)
-		rsnew = np.dot(r, z)
-		p = z + rsnew/rsold * p
-		rsold = rsnew
+			rsnew = np.dot(r, rhat)
+			beta = (alpha/omega)*(rsnew/rsold)
+			p = r + beta(p - omega*Ap)
+			rsold = rsnew
 
-	return x, resPCG
+		return x, resPCG
+	
+	def PBiCGSTAB(self, Afun, Pfun, b):
+		x = np.zeros(np.shape(b))
+		r = b; normb = np.max(np.abs(r))
+		resPCG = [1.0]
+		if normb <= self._thresholdPCG: return
+		rhat = r; p = r
+		rsold = np.dot(r, rhat)
+
+		for i in range(self._nbIter):
+			ptilde = Pfun(p)
+			Aptilde = Afun(ptilde)
+			alpha = rsold/np.dot(Aptilde, rhat)
+			s = r - alpha*Aptilde
+			stilde = Pfun(s)
+			Astilde = Afun(stilde)
+			omega = np.dot(Astilde, s)/np.dot(Astilde, Astilde)
+			x += alpha*ptilde + omega*stilde
+			r = s - omega*Astilde
+
+			resPCG.append(np.max(np.abs(r))/normb)
+			if (resPCG[-1]<=self._thresholdPCG): break
+
+			rsnew = np.dot(r, rhat)
+			beta = (alpha/omega)*(rsnew/rsold)
+			p = r + beta*(p - omega*Aptilde)
+			rsold = rsnew
+
+		return x, resPCG
