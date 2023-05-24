46296d1f8f06c9980455fdba24ea920bd9756d30
JoaquinCornejo <joaquin.cornejo-fuentes@insa-lyon.fr>
2023-05-24 21:14:28 +0200

    add space time in python, not tested

--- src/iga_wq_mf/pysrc/t3dtransient.py
+++ src/iga_wq_mf/pysrc/t3dtransient.py
@@ -83,7 +83,7 @@ if not dataExist:
 
 			# Create a Dirichlet condition
 			Tinout = np.zeros((model.nbctrlpts_total, len(time_list)))
-			for i in range(len(time_list)): Tinout[boundary.thdod, i] = boundary.thDirichletBound[boundary.thdod]
+			for i in range(1, len(time_list)): Tinout[boundary.thdod, i] = boundary.thDirichletBound[boundary.thdod]
 
 			# Add external force 
 			Fend = problem.eval_volForce(powden)
