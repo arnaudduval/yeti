63c6127390626e77591b132d3906ee871a9e5752
DUVAL Arnaud <arnaud.duval@insa-lyon.fr>
2023-07-27 11:58:20 +0200

    Improve FETI example.

--- examples/feti/feti_2.py
+++ examples/feti/feti_2.py
@@ -167,3 +167,105 @@ if True:
     pp.generatevtu(*modeleIGA.get_inputs4postprocVTU(
         'coupling_pcpg',SOL.transpose(),nb_ref=np.array([4,4,1]),
         Flag=np.array([True,True,False])))
+
+
+
+
+# RESULTION Decomposition de Domaine
+time2 = time.time()
+ip1 = np.array([1])
+ip2 = np.array([2])
+ipl = np.array([7,8])
+
+dof1 = np.array([],dtype=np.intp)
+for p in ip1:
+    dof1 = np.concatenate((dof1,np.repeat(np.unique(modeleIGA._IEN[p])-1,3)*3
+                           + np.tile(np.array([0,1,2]),modeleIGA._indCPbyPatch[p].size)))
+dof1 = np.intersect1d(dof1,idof)
+
+K1  = Ktot[dof1,:][:,dof1]
+F1  = Fb[dof1]
+LU1 = sp.linalg.splu(K1)
+
+dof2 = np.array([],dtype=np.intp)
+for p in ip2:
+    dof2 = np.concatenate((dof2,np.repeat(np.unique(modeleIGA._IEN[p])-1,3)*3
+                           + np.tile(np.array([0,1,2]),modeleIGA._indCPbyPatch[p].size)))
+#dof2 = np.intersect1d(dof2,idof)
+K2   = Ktot[dof2,:][:,dof2]
+
+iv2 = manip.get_vertexCPindice(modeleIGA._Nkv,modeleIGA._Jpqr,modeleIGA._dim,num_patch=ip2[0])
+ddlr= np.array([(modeleIGA._indCPbyPatch[ip2[0]][iv2[0]]-1)*3+0,
+                (modeleIGA._indCPbyPatch[ip2[0]][iv2[0]]-1)*3+1,
+                (modeleIGA._indCPbyPatch[ip2[0]][iv2[0]]-1)*3+2,
+                (modeleIGA._indCPbyPatch[ip2[0]][iv2[1]]-1)*3+0,
+                (modeleIGA._indCPbyPatch[ip2[0]][iv2[1]]-1)*3+2,
+                (modeleIGA._indCPbyPatch[ip2[0]][iv2[1]]  )*3+0])
+dof2pp = np.setxor1d(dof2,ddlr)
+K2pp = Ktot[dof2pp,:][:,dof2pp]
+K2pr = Ktot[dof2pp,:][:,ddlr]
+
+ddlr_loc = np.zeros(6,dtype=np.intp)
+for i in np.arange(0,6):
+    ddlr_loc[i] = np.where(dof2 == ddlr[i])[0][0]
+dof2pp_loc = np.setxor1d(np.arange(0,dof2.size),ddlr_loc)
+
+LU2 = sp.linalg.splu(K2pp)
+R2  = np.zeros((dof2.size,6))
+for r in np.arange(0,6):
+    R2[dof2pp_loc,r] = -LU2.solve(K2pr[:,r].toarray().flatten())
+R2[ddlr_loc,:] = np.identity(6)
+
+F2  = Fb[dof2]
+
+
+dofl = np.array([],dtype=np.intp)
+for p in ipl:
+    dofl = np.concatenate((dofl,np.repeat(np.unique(modeleIGA._IEN[p])-1,3)*3
+                           + np.tile(np.array([0,1,2]),modeleIGA._indCPbyPatch[p].size)))
+dofl = np.intersect1d(dofl,idof)
+
+C1 = Ctot[dofl,:][:,dof1]
+C2 = Ctot[dofl,:][:,dof2]
+
+
+def K2star(f2):
+    v2 = np.zeros(dof2.size)
+    v2[dof2pp_loc] = LU2.solve(f2[dof2pp_loc])
+    return v2
+
+def shurOp_wLU(di):
+    ld = di[:-6]
+    al = di[-6:]
+    y1 = LU1.solve(C1.transpose().dot(ld))
+    y2 = K2star(C2.transpose().dot(ld))
+
+    ytot = np.zeros_like(di)
+    ytot[:-6] = C1.dot(y1) + C2.dot(y2) -C2.dot(R2.dot(al))
+    ytot[-6:] = -(C2.dot(R2).transpose()).dot(ld)
+    return ytot
+Sd = sp.linalg.LinearOperator((dofl.size+6,dofl.size+6),matvec=shurOp_wLU, dtype='f')
+
+# interface problem
+t1 = LU1.solve(F1)
+t2 = K2star(F2)
+t = np.zeros(dofl.size+6)
+t[:-6] = C1.dot(t1) + C2.dot(t2)
+t[-6:] = -R2.transpose().dot(F2)
+lmbda,info = sp.linalg.cg(Sd,t,maxiter=50000)
+
+# local solutions
+u1 = LU1.solve(F1-C1.transpose().dot(lmbda[:-6]))
+u2 = K2star(F2-C2.transpose().dot(lmbda[:-6])) + R2.dot(lmbda[-6:])
+
+xDD = np.concatenate((u1,np.concatenate((u2,lmbda[:-6]))))
+
+print('\n Time for DD solving : %.2f s\n\n' % (time.time() - time2))
+
+
+
+# Postprocessing
+SOL,u = rsol.reconstruction(*modeleIGA.get_inputs4solution(xDD))
+pp.generatevtu(*modeleIGA.get_inputs4postprocVTU(
+    'coupling',SOL.transpose(),nb_ref=3*np.array([1,1,1]),
+    Flag=np.array([True,True,False])))
