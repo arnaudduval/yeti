cf8107ed3b9cf7f1e21fc7adcb304bc4732709d2
DUVAL Arnaud <arnaud.duval@insa-lyon.fr>
2023-07-27 10:19:44 +0200

    Add a FETI example.

---
+++ examples/feti/test_feti.py
@@ -0,0 +1,91 @@
+# Python module
+import numpy as np
+import scipy.sparse as sp
+import sys
+import time
+
+#IGA module
+from solver import PCPGortho
+from preprocessing.igaparametrization import IGAparametrization
+from preprocessing.igaparametrization import IGAsubdomain,FETI
+from preprocessing.igaparametrization import IGAmanip    as manip
+from preprocessing.igaparametrization import DDinterface as ddmanip
+import reconstructionSOL as rsol
+import postprocessing.postproc as pp
+
+completeIGA = IGAparametrization(filename='twoplatesDD')
+
+nb_deg = np.zeros((3,completeIGA._nb_patch),dtype=np.intp)
+nb_ref = np.zeros((3,completeIGA._nb_patch),dtype=np.intp)
+additional_knots = {"patches":np.array([]),"1":np.array([]),"2":np.array([]),"3":np.array([])}
+
+p = 2
+r = 2
+# domains
+nb_deg[:2,:2] = p
+nb_ref[:2, 0] = r+1
+nb_ref[:2, 1] = r
+# curves
+nb_ref[0,(2,3)] = r+2
+# lgrge
+nb_deg[0,4] = p
+nb_ref[0,4] = r
+
+# completeIGA.refine(nb_ref,nb_deg,additional_knots)
+completeIGA._NBPINT[ np.where(completeIGA._ELT_TYPE == 'U00') ] = 6
+
+multipleIGA = ddmanip.automaticDomainDecomposition(completeIGA)
+
+dd = []
+count = 0
+for modeleIGA in multipleIGA:
+    dd.append( IGAsubdomain(modeleIGA,count) )
+    count += 1
+
+
+# FETI Analysis
+# -------------
+
+print('-'*4,'\nFETI analysis')
+
+# --
+# factorization
+ti = time.time()
+for domain in dd:
+    domain.set_stiffnessMATRIX()
+    domain.set_couplingMATRIX()
+    domain.set_factorizationMATRIX(tol=1.0e-6)
+    domain.set_admissibleconstMATRIX()
+print(' Local Assembly and Factorization\n (duration : %.2f s).' % (time.time() - ti))
+
+
+# --
+# coarse problem
+ti = time.time()
+tabWeak = []
+for domain in dd:
+    interfaceInfos = domain.get_weakInfos()
+    tabWeak.append(interfaceInfos)
+feti = FETI(tabWeak)
+
+localG = []
+for domain in dd:
+    localG.append(domain._localG)
+feti.set_coarseMATRIX(localG)
+
+localt = []
+locale = []
+for domain in dd:
+    localt.append(domain.compute_condensedRHSvect())
+    locale.append(domain.compute_rigidbodyRHSvect())
+feti.set_RHSt(localt)
+feti.set_RHSe(locale)
+
+
+# print(feti._matrixQ)
+print(feti._matrixG)
+exit()
+feti.solve_coarsePB()
+exit()
+feti.set_projectorP()
+print(' Build and resolution of feti coarse problem\n (duration : %.2f s).' % (time.time() - ti))
