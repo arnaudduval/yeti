a8ca431a691d13db23a4c2b1a71cc6f38782fa6b
JoaquinCornejo <joaquin.cornejo-fuentes@insa-lyon.fr>
2023-08-10 09:26:33 +0200

    improve simulation steady for phd results

--- src/iga_wq_mf/pysrc/phd/test_steadytransfer.py
+++ src/iga_wq_mf/pysrc/phd/test_steadytransfer.py
@@ -1,8 +1,204 @@
 from pysrc.lib.__init__ import *
-from pysrc.lib.lib_load import *
 from pysrc.lib.lib_material import thermomat
 from pysrc.lib.lib_boundary import boundaryCondition
-from pysrc.lib.lib_simulation import encoder, decoder
+from pysrc.lib.lib_geomdl import Geomdl
+from pysrc.lib.lib_part import part
+from pysrc.lib.lib_job import heatproblem
+from pysrc.lib.lib_simulation import steadydecoder
+
+def powden_cube(P: list):
+	""" u = sin(pi*x)*sin(pi*y)*sin(pi*z)
+		f = -div(lambda * grad(u))
+	"""
+	x = P[0, :]
+	y = P[1, :]
+	z = P[2, :]
+
+	# # Isotropy
+	# f = 3*np.pi**2*np.sin(np.pi*x)*np.sin(np.pi*y)*np.sin(np.pi*z) 
+
+	# Anisotropy
+	f = (6*np.pi**2*np.sin(np.pi*x)*np.sin(np.pi*y)*np.sin(np.pi*z) 
+	- (np.pi**2*np.cos(np.pi*x)*np.cos(np.pi*z)*np.sin(np.pi*y))/5 
+	- (np.pi**2*np.cos(np.pi*y)*np.cos(np.pi*z)*np.sin(np.pi*x))/2 
+	- np.pi**2*np.cos(np.pi*x)*np.cos(np.pi*y)*np.sin(np.pi*z)
+	)
+
+	return f
+
+def powden_prism(P: list):
+	""" u = (-5*x+6*y+45)*(5*x+6*y-45)*x*(x-6)*sin(pi*z)
+		f = -div(lambda * grad(u))
+	"""
+	x = P[0, :]
+	y = P[1, :]
+	z = P[2, :]
+
+	# # Isotropy
+	# f = (10*x*np.sin(np.pi*z)*(5*x + 6*y - 45) - 22*x*np.sin(np.pi*z)*(x - 6) 
+	#     - 10*x*np.sin(np.pi*z)*(6*y - 5*x + 45) - 2*np.sin(np.pi*z)*(6*y - 5*x + 45)*(5*x + 6*y - 45) 
+	#     - 10*np.sin(np.pi*z)*(x - 6)*(6*y - 5*x + 45) + 10*np.sin(np.pi*z)*(x - 6)*(5*x + 6*y - 45) 
+	#     + x*np.pi**2*np.sin(np.pi*z)*(x - 6)*(6*y - 5*x + 45)*(5*x + 6*y - 45)
+	# )/1000
+
+	# Anisotropy
+	f = (4*x*np.sin(np.pi*z)*(5*x + 6*y - 45) 
+	- 94*x*np.sin(np.pi*z)*(x - 6) 
+	- 16*x*np.sin(np.pi*z)*(6*y - 5*x + 45) 
+	- 2*np.sin(np.pi*z)*(6*y - 5*x + 45)*(5*x + 6*y - 45) 
+	- 16*np.sin(np.pi*z)*(x - 6)*(6*y - 5*x + 45) 
+	+ 4*np.sin(np.pi*z)*(x - 6)*(5*x + 6*y - 45) 
+	- (np.pi*np.cos(np.pi*z)*(x - 6)*(6*y - 5*x + 45)*(5*x + 6*y - 45))/5 
+	- 4*x*np.pi*np.cos(np.pi*z)*(x - 6)*(6*y - 5*x + 45) 
+	- 2*x*np.pi*np.cos(np.pi*z)*(x - 6)*(5*x + 6*y - 45) 
+	- (x*np.pi*np.cos(np.pi*z)*(6*y - 5*x + 45)*(5*x + 6*y - 45))/5 
+	+ 3*x*np.pi**2*np.sin(np.pi*z)*(x - 6)*(6*y - 5*x + 45)*(5*x + 6*y - 45)
+	)/1000
+	
+	return f
+
+def powden_thickring(P: list):
+	""" u = sin(5*pi*x)*sin(5*pi*y)*sin(5*pi*z)*(x**2+y**2-1)*(x**2+y**2-4)
+		f = -div(lambda * grad(u))
+	"""
+	x = P[0, :]
+	y = P[1, :]
+	z = P[2, :] 
+
+	# # Isotropy
+	# f = (75*np.pi**2*np.sin(5*np.pi*x)*np.sin(5*np.pi*y)*np.sin(5*np.pi*z)*(x**2 + y**2 - 1)*(x**2 + y**2 - 4) 
+	#     - 8*y**2*np.sin(5*np.pi*x)*np.sin(5*np.pi*y)*np.sin(5*np.pi*z) 
+	#     - 4*np.sin(5*np.pi*x)*np.sin(5*np.pi*y)*np.sin(5*np.pi*z)*(x**2 + y**2 - 1) 
+	#     - 4*np.sin(5*np.pi*x)*np.sin(5*np.pi*y)*np.sin(5*np.pi*z)*(x**2 + y**2 - 4) 
+	#     - 8*x**2*np.sin(5*np.pi*x)*np.sin(5*np.pi*y)*np.sin(5*np.pi*z) 
+	#     - 20*x*np.pi*np.cos(5*np.pi*x)*np.sin(5*np.pi*y)*np.sin(5*np.pi*z)*(x**2 + y**2 - 1) 
+	#     - 20*x*np.pi*np.cos(5*np.pi*x)*np.sin(5*np.pi*y)*np.sin(5*np.pi*z)*(x**2 + y**2 - 4) 
+	#     - 20*y*np.pi*np.cos(5*np.pi*y)*np.sin(5*np.pi*x)*np.sin(5*np.pi*z)*(x**2 + y**2 - 1) 
+	#     - 20*y*np.pi*np.cos(5*np.pi*y)*np.sin(5*np.pi*x)*np.sin(5*np.pi*z)*(x**2 + y**2 - 4)
+	# )
+
+	# Anisotropy
+	f = (150*np.pi**2*np.sin(5*np.pi*x)*np.sin(5*np.pi*y)*np.sin(5*np.pi*z)*(x**2 + y**2 - 1)*(x**2 + y**2 - 4)
+	- 16*y**2*np.sin(5*np.pi*x)*np.sin(5*np.pi*y)*np.sin(5*np.pi*z) 
+	- 6*np.sin(5*np.pi*x)*np.sin(5*np.pi*y)*np.sin(5*np.pi*z)*(x**2 + y**2 - 1) 
+	- 6*np.sin(5*np.pi*x)*np.sin(5*np.pi*y)*np.sin(5*np.pi*z)*(x**2 + y**2 - 4) 
+	- 8*x*y*np.sin(5*np.pi*x)*np.sin(5*np.pi*y)*np.sin(5*np.pi*z) 
+	- 8*x**2*np.sin(5*np.pi*x)*np.sin(5*np.pi*y)*np.sin(5*np.pi*z) 
+	- 25*np.pi**2*np.cos(5*np.pi*x)*np.cos(5*np.pi*y)*np.sin(5*np.pi*z)*(x**2 + y**2 - 1)*(x**2 + y**2 - 4) 
+	- 5*np.pi**2*np.cos(5*np.pi*x)*np.cos(5*np.pi*z)*np.sin(5*np.pi*y)*(x**2 + y**2 - 1)*(x**2 + y**2 - 4) 
+	- (25*np.pi**2*np.cos(5*np.pi*y)*np.cos(5*np.pi*z)*np.sin(5*np.pi*x)*(x**2 + y**2 - 1)*(x**2 + y**2 - 4))/2 
+	- 20*x*np.pi*np.cos(5*np.pi*x)*np.sin(5*np.pi*y)*np.sin(5*np.pi*z)*(x**2 + y**2 - 1) 
+	- 10*x*np.pi*np.cos(5*np.pi*y)*np.sin(5*np.pi*x)*np.sin(5*np.pi*z)*(x**2 + y**2 - 1) 
+	- 2*x*np.pi*np.cos(5*np.pi*z)*np.sin(5*np.pi*x)*np.sin(5*np.pi*y)*(x**2 + y**2 - 1) 
+	- 20*x*np.pi*np.cos(5*np.pi*x)*np.sin(5*np.pi*y)*np.sin(5*np.pi*z)*(x**2 + y**2 - 4) 
+	- 10*x*np.pi*np.cos(5*np.pi*y)*np.sin(5*np.pi*x)*np.sin(5*np.pi*z)*(x**2 + y**2 - 4) 
+	- 2*x*np.pi*np.cos(5*np.pi*z)*np.sin(5*np.pi*x)*np.sin(5*np.pi*y)*(x**2 + y**2 - 4) 
+	- 10*y*np.pi*np.cos(5*np.pi*x)*np.sin(5*np.pi*y)*np.sin(5*np.pi*z)*(x**2 + y**2 - 1) 
+	- 40*y*np.pi*np.cos(5*np.pi*y)*np.sin(5*np.pi*x)*np.sin(5*np.pi*z)*(x**2 + y**2 - 1) 
+	- 5*y*np.pi*np.cos(5*np.pi*z)*np.sin(5*np.pi*x)*np.sin(5*np.pi*y)*(x**2 + y**2 - 1) 
+	- 10*y*np.pi*np.cos(5*np.pi*x)*np.sin(5*np.pi*y)*np.sin(5*np.pi*z)*(x**2 + y**2 - 4) 
+	- 40*y*np.pi*np.cos(5*np.pi*y)*np.sin(5*np.pi*x)*np.sin(5*np.pi*z)*(x**2 + y**2 - 4) 
+	- 5*y*np.pi*np.cos(5*np.pi*z)*np.sin(5*np.pi*x)*np.sin(5*np.pi*y)*(x**2 + y**2 - 4)
+	)
+
+	return f
+
+class simulate():
+
+	def __init__(self, simuArgs:dict):
+
+		self._name 		= simuArgs.get('name', '').lower()
+		self._degree 	= simuArgs.get('degree', 2)
+		self._nbcuts 	= simuArgs.get('nb_refinementByDirection', 2)
+		self._nbctrlpts = int(self._degree + 2**self._nbcuts) 
+		
+		self._isGaussQuad = simuArgs.get('isGauss', False)
+		self._funPowDen   = simuArgs.get('funPowerDensity', None)
+		self._iterMethods = simuArgs.get('IterMethods', ['WP'])
+		
+		self._filename = simuArgs.get('folder', './') + self.__make_filename() 
+		
+		return
+	
+	def __make_filename(self):
+		" Make up filename using input information "
+		# Get text file name
+		filename = (self._name 
+					+ '_p_' + str(self._degree) 
+					+ '_nbel_' + str(2**self._nbcuts)
+		)
+		if self._isGaussQuad: filename += '_IGAG'
+		else: filename += '_IGAWQ'
+		filename += '.txt'
+		return filename
+
+	def write_resultsFile(self, inputs:dict): 
+		" Writes and exports simulation data in .txt file "
+		
+		nbIterPCG   = inputs['nbIterPCG']
+		iterMethods = inputs['iterMethods']
+		timeNoIter  = inputs['timeNoIter']
+		timeIter    = inputs['timeIter']
+		residuePCG  = inputs['resPCG']
+		
+		with open(self._filename, 'w') as f:
+			f.write('** RESULTS **\n')
+			f.write('** Iterative solver ' + ','.join([item for item in iterMethods]) + '\n')
+			f.write('** Number of iterations ' + '{:d}\n'.format(nbIterPCG))
+
+			for i, method in enumerate(iterMethods):
+				f.write('**' + method + '\n')
+				f.write('*Time prepare ' + method +'\n')
+				f.write('{:E}\n'.format(timeNoIter[i]))
+				f.write('*Time iter ' + method +'\n')
+				f.write('{:E}\n'.format(timeIter[i]))
+				f.write('*Residue ' + method + '\n')
+				f.writelines(['{:E}'.format(res) + '\n'
+								for res in residuePCG[i]]) 
+		return
+	
+	def __run_iterativeSolver(self, problem:heatproblem, Fext):
+		" Solve steady heat problems using iterative solver "
+		start = time.process_time()
+		sol, residue = problem.solveSteadyHeatProblemFT(Fext)
+		stop = time.process_time()
+		time_t = stop - start 
+		return sol, residue, time_t
+
+	def simulate(self, material:thermomat, boundary:boundaryCondition, overwrite=True):
+		" Runs simulation using given input information "
+
+		geoArgs  = {'name':self._name, 
+					'nb_refinementByDirection': self._nbcuts*np.ones(3, dtype=int),
+					'degree': self._degree*np.ones(3, dtype=int)}
+		modelgeo = Geomdl(geoArgs)
+		modelIGA = modelgeo.getIGAParametrization()
+		if self._isGaussQuad: quadArgs = {'quadrule': 'iga'}
+		else:                 quadArgs = {'quadrule': 'wq'}
+		modelPhy = part(modelIGA, quadArgs)
+
+		problem = heatproblem(material, modelPhy, boundary)
+		Fext    = problem.eval_volForce(self._funPowDen)
+
+		# Run iterative methods
+		timeNoIter = []; problem.addSolverConstraints({'nbIterationsPCG':0})
+		for im in self._iterMethods:
+			problem._methodPCG = im
+			time_temp = self.__run_iterativeSolver(problem, Fext=Fext)[-1]
+			timeNoIter.append(time_temp)
+
+		timeIter, resPCG = [], []; problem.addSolverConstraints({})
+		for im in self._iterMethods:
+			problem._methodPCG = im
+			un, residue_t, time_temp = self.__run_iterativeSolver(problem, Fext=Fext)
+			timeIter.append(time_temp)
+			resPCG.append(residue_t)
+				
+		# Write file
+		output = {'nbIterPCG': problem._nbIterPCG, 'iterMethods': self._iterMethods, 'timeNoIter':timeNoIter, 'timeIter': timeIter, 'resPCG': resPCG}
+		if overwrite: self.write_resultsFile(output)
+
+		return un
 
 # Select folder
 full_path = os.path.realpath(__file__)
@@ -12,7 +208,7 @@ if not os.path.isdir(folder): os.mkdir(folder)
 dataExist   = True
 degree_list = np.arange(6, 7)
 cuts_list   = np.arange(4, 5)
-name_list   = ['cb', 'vb']
+name_list   = ['cb', 'vb', 'tr']
 IterMethods = ['WP', 'C', 'JMC', 'TDC']
 
 def setKprop(P:list):
@@ -33,27 +229,22 @@ for cuts in cuts_list:
 	for degree in degree_list:
 		for name in name_list: 
 
-			if name   == 'cb' : funpow, funtemp = powden_cube, None 
-			elif name == 'vb' : funpow, funtemp = powden_prism, None 
-			elif name == 'tr' : funpow, funtemp = powden_thickring, None 
-			elif name == 'rqa': funpow, funtemp = powden_rotring, temperature_rotring 
+			if name   == 'cb' : funpow = powden_cube 
+			elif name == 'vb' : funpow = powden_prism 
+			elif name == 'tr' : funpow = powden_thickring 
 
 			inputs = {'degree': degree, 'nb_refinementByDirection': cuts, 'name': name, 'isGauss': False, 
-					'funPowerDensity': funpow, 'funTemperature': funtemp, 'IterMethods': IterMethods,
-					'folder': folder}
-			simu   = encoder(inputs)  
+					'funPowerDensity': funpow, 'IterMethods': IterMethods, 'folder': folder}
+			simulation = simulate(inputs)  
 			
 			if not dataExist:
 				mat = thermomat()
 				mat.addConductivity(setKprop, isIsotropic=False)				
-				table    = np.ones((3, 2), dtype=bool)
-				boundary = boundaryCondition(simu._nbctrlpts*np.ones(3, dtype=int))
-				boundary.add_DirichletConstTemperature(table=table)
-				un = simu.simulate(material=mat, boundary=boundary, overwrite=True)
-				np.save(folder+'solution', un)
+				boundary = boundaryCondition(simulation._nbctrlpts*np.ones(3, dtype=int))
+				boundary.add_DirichletConstTemperature(table=np.ones((3, 2), dtype=bool))
+				simulation.simulate(material=mat, boundary=boundary, overwrite=True)
 
 			else :
-				filename   = simu._filename
-				simuOutput = decoder(filename)
+				simuOutput = steadydecoder(simulation._filename)
 				simuOutput.plot_results(extension='.png', plotLegend=True)
 				
