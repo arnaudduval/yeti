1a3479e09ed11f3e5d0215d660c6ebd9d8c46f2c
DUVAL Arnaud <arnaud.duval@insa-lyon.fr>
2023-10-26 14:37:00 +0200

    Improve code style

--- benchs/load_combination/embeddedbeam_load_combination.py
+++ benchs/load_combination/embeddedbeam_load_combination.py
@@ -26,18 +26,14 @@ Test to verify if load sum is correct in force vector
 """
 
 import sys
-import time
 import random
 import copy
 
 import numpy as np
 import scipy.sparse as sp
 
-import reconstructionSOL as rsol
 from preprocessing.igaparametrization import IGAparametrization
 from stiffmtrx_elemstorage import sys_linmat_lindef_static as build_stiffmatrix
-import postprocessing.postproc as pp
-
 
 if __name__ == '__main__':
     cases = []
@@ -103,22 +99,22 @@ if __name__ == '__main__':
     # Force vectors
     f_vects = {}
 
-    def matrix_assembly(iga_model):
+    def matrix_assembly(model):
         """
         Assemble stiffness matrix and force vector for a given IGA model
         """
-        ndof = iga_model._nb_dof_free
-        idof = iga_model._ind_dof_free[:ndof]-1
-        data, row, col, Fb = build_stiffmatrix(
-                        *iga_model.get_inputs4system_elemStorage())
-        Kside = sp.coo_matrix((data, (row, col)),
-                      shape=(iga_model._nb_dof_tot, iga_model._nb_dof_tot),
-                      dtype='float64').tocsc()
-        Ktot = Kside + Kside.transpose()
+        ndof = model.nb_dof_free
+        idof = model.ind_dof_free[:ndof]-1
+        data, row, col, fvect = build_stiffmatrix(
+                        *model.get_inputs4system_elemStorage())
+        kside = sp.coo_matrix((data, (row, col)),
+                              shape=(iga_model.nb_dof_tot, model.nb_dof_tot),
+                              dtype='float64').tocsc()
+        ktot = kside + kside.transpose()
 
-        del Kside, data, row, col
+        del kside, data, row, col
 
-        return copy.copy(Ktot), copy.copy(Fb), copy.copy(idof)
+        return copy.copy(ktot), copy.copy(fvect), copy.copy(idof)
 
     # Movable control points coordinates to introduce non linearity
     # Array : index of cp, index of coordinate
@@ -126,7 +122,8 @@ if __name__ == '__main__':
                         [9, 0], [10, 1], [10, 0], [11, 0], [12, 2], [12, 0],
                         [13, 1], [13, 2], [13, 0], [14, 2], [14, 0], [15, 0],
                         [16, 1], [16, 0], [17, 0],
-                        [19, 1], [21, 2], [22, 1], [22, 2], [23, 2], [25, 1],   # End of hull
+                        [19, 1], [21, 2], [22, 1], [22, 2], [23, 2], [25, 1],
+                        # End of hull
                         [28, 0], [30, 1], [31, 0], [31, 1], [32, 1], [34, 0],
                         [36, 2], [37, 0], [37, 2], [38, 2], [39, 1], [39, 2],
                         [40, 0], [40, 1], [40, 2], [41, 1], [41, 2], [42, 2],
@@ -134,21 +131,18 @@ if __name__ == '__main__':
                         [46, 0], [48, 1], [49, 0], [49, 1], [50, 1], [52, 0]])
 
     for case in cases:
-        iga_model = IGAparametrization(filename='inputs_embeddedbeam/'+case['filename'])
+        iga_model = IGAparametrization(filename='inputs_embeddedbeam/' +
+                                       case['filename'])
 
         # Move control points
         random.seed(3)
+        # Hull size
+        hull_size = np.array([20., 3., 1.])
         # Move hull CPs
-        # TODO dimensions of hull(20, 3, 1) should be handled as variable
         for move in movable[:movable.shape[0]//2]:
             # CP at index move[0] is moved in move[1] direction
-            match move[1]:
-                case 0:
-                    iga_model.coords[move[1], move[0]] = 20.*(0.1 + 0.8*random.random())
-                case 1:
-                    iga_model.coords[move[1], move[0]] = 3.*(0.1 + 0.8*random.random())
-                case 2:
-                    iga_model.coords[move[1], move[0]] = 0.1 + 0.8*random.random()
+            iga_model.coords[move[1], move[0]] = \
+                hull_size[move[1]]*(0.1 + 0.8*random.random())
 
         # Move embedded entity CPs
         for move in movable[movable.shape[0]//2:]:
@@ -163,85 +157,86 @@ if __name__ == '__main__':
     # Tests on force vectors
     # 1 - ensure that F2F4 is the sum of F2 and F4
     error = np.linalg.norm(f_vects['embeddedbeam-dloadF2F4'] -
-                        f_vects['embeddedbeam-dloadF2'] -
-                        f_vects['embeddedbeam-dloadF4'])
+                           f_vects['embeddedbeam-dloadF2'] -
+                           f_vects['embeddedbeam-dloadF4'])
     print(error)
     if error > 1.e-12:
         sys.exit(-1)
 
     # 2 - ensure that F4F2 equal to F2F4
     error = np.linalg.norm(f_vects['embeddedbeam-dloadF2F4'] -
-                        f_vects['embeddedbeam-dloadF4F2'])
+                           f_vects['embeddedbeam-dloadF4F2'])
     print(error)
     if error > 1.e-12:
         sys.exit(-1)
 
     # 3 - ensure that F2 centrif is the sum of F2 and centrif
     error = np.linalg.norm(f_vects['embeddedbeam-dloadF2-centrif'] -
-                        f_vects['embeddedbeam-dloadF2'] -
-                        f_vects['embeddedbeam-centrif'])
+                           f_vects['embeddedbeam-dloadF2'] -
+                           f_vects['embeddedbeam-centrif'])
     print(error)
     if error > 1.e-12:
         sys.exit(-1)
 
     # 4 - ensure that centrif F2 equal F2 centrif
     error = np.linalg.norm(f_vects['embeddedbeam-dloadF2-centrif'] -
-                        f_vects['embeddedbeam-centrif-dloadF2'])
+                           f_vects['embeddedbeam-centrif-dloadF2'])
     print(error)
     if error > 1.e-12:
         sys.exit(-1)
 
     # 5 - ensure that F4 centrif is the sum of F4 and centrif
     error = np.linalg.norm(f_vects['embeddedbeam-dloadF4-centrif'] -
-                        f_vects['embeddedbeam-dloadF4'] -
-                        f_vects['embeddedbeam-centrif'])
+                           f_vects['embeddedbeam-dloadF4'] -
+                           f_vects['embeddedbeam-centrif'])
     print(error)
     if error > 1.e-12:
         sys.exit(-1)
 
     # 6 - ensure that centrif F4 equal F4 centrif
     error = np.linalg.norm(f_vects['embeddedbeam-dloadF4-centrif'] -
-                        f_vects['embeddedbeam-centrif-dloadF4'])
+                           f_vects['embeddedbeam-centrif-dloadF4'])
     print(error)
     if error > 1.e-12:
         sys.exit(-1)
 
     # 7 - ensure that F2 F4 centrif, is the sum of F2, F4 and centrif
     error = np.linalg.norm(f_vects['embeddedbeam-dloadF2-dloadF4-centrif'] -
-                        f_vects['embeddedbeam-dloadF2'] -
-                        f_vects['embeddedbeam-dloadF4'] -
-                        f_vects['embeddedbeam-centrif'])
+                           f_vects['embeddedbeam-dloadF2'] -
+                           f_vects['embeddedbeam-dloadF4'] -
+                           f_vects['embeddedbeam-centrif'])
     print(error)
     if error > 1.e-12:
         sys.exit(-1)
 
-    # 8 - ensure the sum works whatever the order of declaration of the 3 loadings
+    # 8 - ensure the sum works whatever the order of declaration
+    # of the 3 loadings
     error = np.linalg.norm(f_vects['embeddedbeam-dloadF2-dloadF4-centrif'] -
-                        f_vects['embeddedbeam-dloadF4-dloadF2-centrif'])
-    print(error)
-    if error > 1.e-12:
-        sys.exit(-1)
-
-    error = np.linalg.norm(f_vects['embeddedbeam-dloadF2-dloadF4-centrif'] -
-                        f_vects['embeddedbeam-dloadF2-centrif-dloadF4'])
+                           f_vects['embeddedbeam-dloadF4-dloadF2-centrif'])
     print(error)
     if error > 1.e-12:
         sys.exit(-1)
 
     error = np.linalg.norm(f_vects['embeddedbeam-dloadF2-dloadF4-centrif'] -
-                        f_vects['embeddedbeam-centrif-dloadF2-dloadF4'])
+                           f_vects['embeddedbeam-dloadF2-centrif-dloadF4'])
     print(error)
     if error > 1.e-12:
         sys.exit(-1)
 
     error = np.linalg.norm(f_vects['embeddedbeam-dloadF2-dloadF4-centrif'] -
-                        f_vects['embeddedbeam-centrif-dloadF4-dloadF2'])
+                           f_vects['embeddedbeam-centrif-dloadF2-dloadF4'])
     print(error)
     if error > 1.e-12:
         sys.exit(-1)
 
     error = np.linalg.norm(f_vects['embeddedbeam-dloadF2-dloadF4-centrif'] -
-                        f_vects['embeddedbeam-dloadF4-centrif-dloadF2'])
+                           f_vects['embeddedbeam-centrif-dloadF4-dloadF2'])
+    print(error)
+    if error > 1.e-12:
+        sys.exit(-1)
+
+    error = np.linalg.norm(f_vects['embeddedbeam-dloadF2-dloadF4-centrif'] -
+                           f_vects['embeddedbeam-dloadF4-centrif-dloadF2'])
     print(error)
     if error > 1.e-12:
         sys.exit(-1)
