bc41492ea3fa349da98403b8de9f6b6f20548ec3
DUVAL Arnaud <arnaud.duval@insa-lyon.fr>
2023-11-28 14:18:20 +0100

    Add bench cases for VTK Bezier output. No CTets is made and coupled case must be corrected.

--- benchs/vtk_bezier_output/linear_analysis_2patchs_coupling_U5.py
+++ benchs/vtk_bezier_output/linear_analysis_2patchs_coupling_U5.py
@@ -20,8 +20,8 @@
 # -*- coding: utf-8 -*-
 
 """
-Generate VTU output using Bezier for a strong (common control points)
-assembly of 2 patchs.
+Generate VTU output using Bezier en assembly of 2 patchs with Mortar
+coupling
 No test is made on the content of output files.
 """
 
@@ -30,23 +30,32 @@ import scipy.sparse as sp
 
 from preprocessing.igaparametrization import IGAparametrization
 from stiffmtrx_elemstorage import sys_linmat_lindef_static as build_stiffmatrix
+from coupling.cplgmatrix import cplg_matrixu5 as cplg_matrixU5
 import reconstructionSOL as rsol
 import postprocessing.postproc as pp
 
 # modeleIGA = IGAparametrization(filename='BentPipeQuarter')
-modeleIGA = IGAparametrization(filename='inputs/BentPipe_2patchs')
+modeleIGA = IGAparametrization(filename='inputs/BentPipe_2patchs_coupling_U5')
 
 # Model refinement
 nb_ref = np.zeros((3, modeleIGA.nb_patch), dtype=np.intp)
 nb_deg = np.zeros((3, modeleIGA.nb_patch), dtype=np.intp)
 
 
-nb_ref[:, 0] = np.array([3, 2, 3])
-nb_ref[:, 1] = np.array([3, 2, 3])
+nb_ref[:, 0] = np.array([1, 1, 2])
+nb_ref[:, 1] = np.array([1, 1, 2])
 
-nb_deg[:, 0] = np.array([0, 1, 0])
-nb_deg[:, 1] = np.array([0, 1, 0])
+nb_deg[:, 0] = np.array([1, 2, 1])  # Degree 3 in all directions
+nb_deg[:, 1] = np.array([1, 2, 1])  # Degree 3 in all directions
 
+# Coupling patches
+nb_ref[:, 2] = np.array([1, 2, 0])
+nb_ref[:, 3] = np.array([1, 2, 0])
+
+nb_deg[:, 2] = np.array([2, 2, 0])  # Degree 2
+nb_deg[:, 3] = np.array([2, 2, 0])  # Degree 2
+
+print(modeleIGA.nb_patch)
 modeleIGA.refine(nb_ref, nb_deg)
 
 # Matrix assembly
@@ -62,15 +71,30 @@ Kside = sp.coo_matrix((data, (row, col)),
                       shape=(modeleIGA._nb_dof_tot, modeleIGA._nb_dof_tot),
                       dtype='float64').tocsc()
 Ktot = Kside + Kside.transpose()
-K2solve = Ktot[idof, :][:, idof]
 del Kside, data, row, col
 
+
+print("Build coupling matrix")
+Cdata, Crow, Ccol = cplg_matrixU5( *modeleIGA.get_inputs4cplgmatrixU5() )
+Cside = sp.coo_matrix((Cdata,(Crow,Ccol)), shape=(modeleIGA._nb_dof_tot,modeleIGA._nb_dof_tot),
+                      dtype='float64').tocsc()
+Ctot  = Cside + Cside.transpose()
+del Cdata,Crow,Ccol,Cside
+
+
 # Resolution
-x = sp.linalg.spsolve(K2solve, Fb[idof])
+K2solve = Ktot[idof,:][:,idof]
+C2solve = Ctot[idof,:][:,idof] * K2solve.max()
+x = sp.linalg.spsolve(K2solve + C2solve, Fb[idof])
 
 # Solution reconstruction
 SOL, u = rsol.reconstruction(**modeleIGA.get_inputs4solution(x))
 
+pp.generatevtu(*modeleIGA.get_inputs4postprocVTU(
+    'couplingU5',SOL.transpose(),nb_ref=np.array([3,3,3]),
+    Flag=np.array([True,True,False])))
+
+exit()
 
 # VTK output using Bezier elements
 for i_patch in range(modeleIGA._nb_patch):
