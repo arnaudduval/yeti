c3932347f9108bb51d2127b534fbb2793b9d0566
JoaquinCornejo <joaquin.cornejo-fuentes@insa-lyon.fr>
2024-07-04 08:53:40 +0200

    testing

--- src/iga_wq_mf/analysisThesis/t2dplasticity.py
+++ src/iga_wq_mf/analysisThesis/t2dplasticity.py
@@ -28,13 +28,15 @@ def run(folder=None):
 
 # Set global variables
 GEONAME = 'SQ'
-TRACTION = 150.0
+TRACTION = 400.0
 YOUNG, POISSON = 2500, 0.25
 NBSTEPS = 101
-TIME_LIST = np.linspace(0, np.pi, NBSTEPS)
+TIME_LIST = np.linspace(0, np.pi/2, NBSTEPS)
 MATARGS = {'elastic_modulus':YOUNG, 'elastic_limit':5, 'poisson_ratio': POISSON, 
-			'isoHardLaw': {'name':'linear', 'Eiso':0.0}, 'kineHardLaw':{'parameters':np.array([[500, 0]])}}
-isReference = True
+			'isoHardLaw': {'name':'linear', 'Eiso':0.0}, 
+			'kineHardLaw':{'parameters':np.array([[500, 0]])}
+			}
+isReference = False
 
 def forceSurf_infPlate(P:list):
 	x = P[0, :]; y = P[1, :]; nnz = np.size(P, axis=1)
@@ -43,7 +45,7 @@ def forceSurf_infPlate(P:list):
 	F[1, :] = -TRACTION*(np.min(tmp, axis=0))**2
 	return F
 
-def simulate(degree, cuts, quadArgs, step=-2):
+def simulate(degree, cuts, quadArgs):
 	geoArgs = {'name': 'SQ', 'degree': degree*np.ones(3, dtype=int), 
 				'nb_refinementByDirection': cuts*np.ones(3, dtype=int), 
 				'extra':{'XY':np.array([[-1.0, -1.0], [1.0, -1.0], [1.0, 1.0], [-1.0, 1.0]])}
@@ -68,64 +70,89 @@ def simulate(degree, cuts, quadArgs, step=-2):
 	Fext_list = np.zeros((2, modelPhy.nbctrlpts_total, NBSTEPS))
 	for k in range(len(TIME_LIST)): Fext_list[:, :, k] = np.sin(TIME_LIST[k])*Fref
 	displacement = np.zeros(np.shape(Fext_list))
-	_, internalVars = problem.solveElastoPlasticityProblem(displacement, Fext_list[:, :, :step+1])
-	return problem, displacement[:, :, :step+1], meshparam, internalVars
+	_, internalVars = problem.solveElastoPlasticityProblem(displacement, Fext_list)
+	return problem, displacement, meshparam, internalVars
 
 if isReference:
-	degree, cuts = 1, 6
+	degree, cuts = 2, 8
 	quadArgs = {'quadrule': 'iga', 'type': 'leg'}
 	problem, displacement, _, internalVars = simulate(degree, cuts, quadArgs)
-	np.save(folder + 'disppl', displacement)
-	with open(folder + 'refpartpl.pkl', 'wb') as outp:
-		pickle.dump(problem.part, outp, pickle.HIGHEST_PROTOCOL)
+	# np.save(folder + 'disppl', displacement)
+	# with open(folder + 'refpartpl.pkl', 'wb') as outp:
+	# 	pickle.dump(problem.part, outp, pickle.HIGHEST_PROTOCOL)
+
 	stress_qp = internalVars.get('stress', None)
-	for j, i in enumerate(range(0, NBSTEPS-1, 4)):
+	alpha_qp = internalVars.get('hardening', None)
+	plastic_qp = np.where(np.abs(alpha_qp)<1e-6, 0.0, 1.0)
+	for j, i in enumerate(range(0, NBSTEPS, 4)):
 		devstress_qp = computeDeviatoric4All(stress_qp[:, :, i], dim=2)
 		vonMises_qp = np.sqrt(3/2)*computeSymTensorNorm4All(devstress_qp, dim=2)
 		vonMises_cp = problem.L2projectionCtrlpts(vonMises_qp)
-		problem.part.exportResultsCP(fields={'stress': vonMises_cp}, name='pls'+str(j), folder=folder)
+		alpha_cp = problem.L2projectionCtrlpts(alpha_qp[0, :, i])
+		plastic_cp = problem.L2projectionCtrlpts(plastic_qp[0, :, i])
+		problem.part.exportResultsCP(fields={'stress': vonMises_cp, 'straineq': alpha_cp, 'plastic':plastic_cp}, 
+									name='pls'+str(j), folder=folder, sampleSize=401)
 	run(folder=folder)
 	
 else:
-
-	degree_list = np.array([1, 2, 3])
-	cuts_list  = np.arange(2, 6)
-	step_list  = range(0, NBSTEPS, 4)
-	error_list = np.ones((len(step_list), len(degree_list), len(cuts_list)))
-	mesh_list  = np.ones((len(degree_list), len(cuts_list)))
-
+	
 	with open(folder + 'refpartpl.pkl', 'rb') as inp:
 		part_ref = pickle.load(inp)
 	disp_ref = np.load(folder + 'disppl.npy')
-	quadArgs = {'quadrule': 'iga', 'type': 'leg'}
-	
-	for i, degree in enumerate(degree_list):
-		for j, cuts in enumerate(cuts_list):
-			problem, displacement, meshparam, _= simulate(degree, cuts, quadArgs)
-			mesh_list[i, j] = meshparam
 
-			for k, step in enumerate(step_list):
-				error_list[k, i, j], _ = problem.normOfError(displacement[:, :, step], 
-														normArgs={'type':'H1', 
-														'part_ref':part_ref, 
-														'u_ref': disp_ref[:, :, step]})
+	normalPlot  = {'marker': 's', 'linestyle': '-', 'markersize': 10}
+	onlyMarker1 = {'marker': 'o', 'linestyle': '--', 'markersize': 6}
 
-	np.save(folder + 'plasticity2D', error_list)
-	np.save(folder + 'meshpar2D', mesh_list)
+	degree_list = np.array([1, 2, 3, 4])
+	cuts_list  = np.arange(2, 7)
+	step_list  = range(1, NBSTEPS, 3)
 
-	error_list = np.load(folder + 'plasticity2D.npy')
-	mesh_list = np.load(folder+'meshpar2D.npy')
+	# # quadArgs = {'quadrule': 'iga', 'type': 'leg'}
+	# quadArgs = {'quadrule': 'wq', 'type': 1}
+	# error_list = np.ones((len(step_list), len(degree_list), len(cuts_list)))
+	# mesh_list  = np.ones((len(degree_list), len(cuts_list)))
+	# for i, degree in enumerate(degree_list):
+	# 	for j, cuts in enumerate(cuts_list):
+	# 		problem, displacement, meshparam, _= simulate(degree, cuts, quadArgs)
+	# 		mesh_list[i, j] = meshparam
+
+	# 		for k, step in enumerate(step_list):
+	# 			error_list[k, i, j], _ = problem.normOfError(displacement[:, :, step], 
+	# 													normArgs={'type':'H1', 
+	# 													'part_ref':part_ref, 
+	# 													'u_ref': disp_ref[:, :, step]})
+
+	# quadrule = quadArgs['quadrule']; quadtype = quadArgs['type']
+	# np.save(folder + 'plasticity2D'+quadrule+str(quadtype), error_list)
+	# np.save(folder + 'meshpar2D'+quadrule+str(quadtype), mesh_list)
 
 	for k, step in enumerate(step_list):
-		fig, ax = plt.subplots(figsize=(9,6))
-		for i, degree in enumerate(degree_list):
-			color = COLORLIST[i]
-			ax.loglog(mesh_list[i, :], error_list[k, i, :], color=color, marker='o', markerfacecolor='w',
-						markersize=10, linestyle='-', label='degree ' + r'$p=\,$' + str(degree))
-		ax.set_ylabel(r'$\displaystyle\frac{||u - u^h||_{H_1(\Omega)}}{||u||_{H_1(\Omega)}}$')
+		fig, ax = plt.subplots(figsize=(6, 5))
+		for quadrule, quadtype, plotpars in zip(['iga', 'wq'], ['leg', 1], [normalPlot, onlyMarker1]):
+			error_list = np.ones(len(cuts_list))
+			error_list = np.load(folder + 'plasticity2D'+quadrule+str(quadtype)+'.npy')
+			mesh_list = np.load(folder+'meshpar2D'+quadrule+str(quadtype)+'.npy')
+		
+			for i, degree in enumerate(degree_list):
+				color = COLORLIST[i]
+				if quadrule == 'iga': 
+					ax.loglog(mesh_list[i, :], error_list[k, i, :], label='IGA-GL deg. '+str(degree), color=color, marker=plotpars['marker'], markerfacecolor='w',
+							markersize=plotpars['markersize'], linestyle=plotpars['linestyle'])
+					# slope = round(np.polyfit(np.log(mesh_list[i, 2:]), np.log(error_list[k, i, 2:]), 1)[0], 1)
+					# annotation.slope_marker((mesh_list[i, -1],  error_list[k, i, -1]), slope, 
+					# 				poly_kwargs={'facecolor': (0.73, 0.8, 1)}, ax=ax)
+				else: 
+					ax.loglog(mesh_list[i, :], error_list[k, i, :], color=color, marker=plotpars['marker'], markerfacecolor='w',
+						markersize=plotpars['markersize'], linestyle=plotpars['linestyle'])
+
+		ax.loglog([], [], color='k', marker=onlyMarker1['marker'], markerfacecolor='w',
+				markersize=onlyMarker1['markersize'], linestyle=onlyMarker1['linestyle'], label="IGA-WQ 4")
+
+		ax.set_ylabel(r'$\displaystyle ||u - u^h||_{H^1(\Omega)}$')
 		ax.set_xlabel('Mesh parameter ' + r'$h_{max}$')
-		ax.set_ylim(top=1, bottom=1e-10)
-		ax.set_xlim(left=5e-2, right=1e0)
-		ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+		ax.set_ylim(top=1, bottom=1e-8)
+		ax.set_xlim(left=2e-2, right=1e0)
+		ax.legend(loc='lower right')
 		fig.tight_layout()
-		fig.savefig(folder + 'FigConvergencePlasticityAllH1' + str(step) +  GEONAME + '.pdf')
+		fig.savefig(folder + 'FigConvergencePlasticity' + str(step) +  GEONAME + '.png')
+		plt.close(fig=fig)
