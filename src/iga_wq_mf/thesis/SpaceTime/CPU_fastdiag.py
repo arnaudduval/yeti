661f866344c3a7786105fa0e1931bdbe15f5b8a0
JoaquinCornejo <joaquin.cornejo-fuentes@insa-lyon.fr>
2024-07-18 17:21:00 +0200

    add cost of space time

---
+++ src/iga_wq_mf/thesis/SpaceTime/CPU_fastdiag.py
@@ -0,0 +1,69 @@
+from thesis.SpaceTimeHeat.__init__ import *
+from pysrc.lib.lib_base import createUniformKnotvector_Rmultiplicity
+from pysrc.lib.lib_quadrules import WeightedQuadrature
+
+# Set global variables
+TODOSIMU = False
+degList = np.arange(1, 6)
+cutList = np.arange(4, 8)
+NDIM = 4
+filename = FOLDER2SAVE + 'sptFD_time'
+
+if TODOSIMU:
+
+	timeMatrix = np.zeros((len(cutList)+1, len(degList)+1))
+	timeMatrix[1:, 0] = np.array([2**i for i in cutList])
+	timeMatrix[0, 1:] = degList
+
+	for i, cuts in enumerate(cutList):
+		for j, degree in enumerate(degList):
+			nbel = 2**cuts
+			knotvector = createUniformKnotvector_Rmultiplicity(degree, nbel)
+			weightedQuad = WeightedQuadrature(degree, knotvector, quadArgs={})
+			info = weightedQuad.getQuadratureRulesInfo()
+			qp, indices, dersbasis, dersweights = info; nbqp = len(qp)
+			nb_ctrlpts = weightedQuad.nbctrlpts
+			vector_in = np.random.random(nb_ctrlpts**NDIM)
+
+			inpts = [*[nbqp for i in range(NDIM)], *[indices[j] for i in range(NDIM) for j in range(2)], 
+					*[dersbasis for i in range(NDIM)], *[dersweights for i in range(NDIM)]]
+
+			start = time.process_time()
+			if NDIM == 3: eigensolver.sptfastdiagonalization_2d(*inpts, vector_in)
+			if NDIM == 4: eigensolver.sptfastdiagonalization_3d(*inpts, vector_in)
+			finish = time.process_time()
+			timeElapsed = finish - start
+
+			print('For p = %s, nbel = %s, time: %.4f' %(degree, nbel, timeElapsed))
+			timeMatrix[i+1, j+1] = timeElapsed
+			np.savetxt(filename+'.dat', timeMatrix)
+
+# Load data
+file = np.loadtxt(filename+'.dat')
+degList = file[0, 1:]; nbelList = file[1:, 0]; timeElapsed = file[1:, 1:]
+
+fig, ax = plt.subplots(figsize=(5, 4))
+for i, degree in enumerate(degList): 
+	ax.loglog(nbelList**4, timeElapsed[:, i], label=r'$p_s=p_t=$' + str(int(degree)), marker='s', color='k', alpha=(i+1)/len(degList))
+
+slope = np.polyfit(np.log(nbelList**4), np.log(timeElapsed[:, 2]), 1)[0]
+slope = round(slope, 1)
+annotation.slope_marker((nbelList[-2]**4,  timeElapsed[-2, 2]), slope, 
+				poly_kwargs={'facecolor': (0.73, 0.8, 1)}, ax=ax)
+
+ax.xaxis.set_major_formatter(mpl.ticker.ScalarFormatter())
+ax.xaxis.set_minor_formatter(mpl.ticker.NullFormatter())
+
+if NDIM == 3: ...
+if NDIM == 4: 
+	ax.set_xticks([1e4, 16**4, 32**4, 64**4, 128**4, 1e9])
+	ax.set_xticklabels([r'$10^4$', r'$16^4$', r'$32^4$', r'$64^4$', r'$128^4$', r'$10^9$'])
+
+ax.minorticks_off()
+ax.legend(loc='best')
+ax.set_xlabel('Total number of elements')
+ax.set_ylabel('Wall time (s)')
+ax.set_ylim([1e-2, 1e3])
+ax.set_xlim([1e4, 1e9])
+fig.tight_layout()
+fig.savefig(filename+'.pdf')
---
+++ src/iga_wq_mf/thesis/SpaceTime/CPU_matrixfree.py
@@ -0,0 +1,131 @@
+from thesis.SpaceTimeHeat.__init__ import *
+from pysrc.lib.lib_base import createUniformOpenCurve
+from pysrc.lib.lib_geomdl import Geomdl
+from pysrc.lib.lib_part import part, part1D
+from pysrc.lib.lib_material import heatmat
+from pysrc.lib.lib_boundary import boundaryCondition
+from pysrc.lib.lib_job3d import stheatproblem
+
+# Set global variables
+TODOSIMU = False
+degList = range(1, 7)
+cuts = 4
+# quadArgs = {'quadrule':'wq', 'type':2}
+quadArgs = {'quadrule':'iga', 'type':'leg'}
+
+if TODOSIMU:
+
+	timeMF_Mass = np.zeros((len(degList), 2))
+	timeMF_Mass[:, 0] = degList
+	timeMF_Stiff = np.zeros((len(degList), 2))
+	timeMF_Stiff[:, 0] = degList
+	timeMF_dersMass = np.zeros((len(degList), 2))
+	timeMF_dersMass[:, 0] = degList
+	timeMF_dersStiff = np.zeros((len(degList), 2))
+	timeMF_dersStiff[:, 0] = degList
+
+	for i, degList in enumerate(degList):
+		
+		geoArgs = {'name': 'VB', 'degree': degList*np.ones(3, dtype=int), 
+					'nb_refinementByDirection': cuts*np.ones(3, dtype=int)
+		}
+		blockPrint()			
+		modelGeo = Geomdl(geoArgs)
+		modelIGA = modelGeo.getIGAParametrization()
+		modelPhy = part(modelIGA, quadArgs=quadArgs)
+		timecrv  = part1D(createUniformOpenCurve(degList, int(2**cuts), 1.0), {'quadArgs':quadArgs})
+
+		heatmaterial = heatmat()
+		heatmaterial.addCapacity(inpt=1.0, isIsotropic=True)
+		heatmaterial.addConductivity(inpt=1.0, isIsotropic=True, shape=3)
+		heatmaterial.addCapacityDers(inpt=1.0, isIsotropic=True)
+		heatmaterial.addConductivityDers(inpt=1.0, isIsotropic=True, shape=3)
+	
+		# Set Dirichlet boundaries	
+		sptnbctrlpts = np.array([*modelPhy.nbctrlpts[:modelPhy.dim], timecrv.nbctrlpts_total])
+		boundary = boundaryCondition(sptnbctrlpts)
+		dirichlet_table = np.ones((4, 2)); dirichlet_table[-1, -1] = 0
+		boundary.add_DirichletConstTemperature(table=dirichlet_table)
+		enablePrint()
+
+		# Solve elastic problem
+		problem = stheatproblem(heatmaterial, modelPhy, timecrv, boundary)
+		vector_in = np.random.random(boundary._nbctrlpts_total)
+
+		# ------------------
+		# Compute MF product
+		# ------------------
+		enablePrint()
+		print('******')
+		start = time.process_time()
+		problem.compute_mfSTCapacity(vector_in)
+		finish = time.process_time()
+		print('Time Capacity:%.2e' %(finish-start))
+		timeMF_Mass[i, 1] = finish - start
+
+		start = time.process_time()
+		problem.compute_mfSTConductivity(vector_in)
+		finish = time.process_time()
+		print('Time Conductivity:%.2e' %(finish-start))
+		timeMF_Stiff[i, 1] = finish - start
+
+		start = time.process_time()
+		problem.compute_mfSTCapacityDers(vector_in)
+		finish = time.process_time()
+		print('Time Ders Capacity:%.2e' %(finish-start))
+		timeMF_dersMass[i, 1] = finish - start
+
+		start = time.process_time()
+		problem.compute_mfSTConductivityDers(vector_in)
+		finish = time.process_time()
+		print('Time Ders Conductivity:%.2e' %(finish-start))
+		timeMF_dersStiff[i, 1] = finish - start
+
+		np.savetxt(FOLDER2SAVE+'sptMF_Mass_'+quadArgs['quadrule']+'_'+str(quadArgs['type'])+'.dat', timeMF_Mass)
+		np.savetxt(FOLDER2SAVE+'sptMF_Stiff_'+quadArgs['quadrule']+'_'+str(quadArgs['type'])+'.dat', timeMF_Stiff)
+		np.savetxt(FOLDER2SAVE+'sptMF_MassDers_'+quadArgs['quadrule']+'_'+str(quadArgs['type'])+'.dat', timeMF_dersMass)
+		np.savetxt(FOLDER2SAVE+'sptMF_StiffDers_'+quadArgs['quadrule']+'_'+str(quadArgs['type'])+'.dat', timeMF_dersStiff)
+
+fig, ax = plt.subplots(figsize=(5, 4))
+IgaPlot = {'marker': 's', 'linestyle': '-', 'markersize': 10}
+WQ1Plot = {'marker': 'x', 'linestyle': '--', 'markersize': 6}
+WQ2Plot = {'marker': 'o', 'linestyle': ':', 'markersize': 6}
+plotoptions = [IgaPlot, WQ2Plot]
+sufixList = ['iga_leg', 'wq_2']
+labels = [r'$\mathsf{A}v_{in}$', r'$\mathsf{B}v_{in}$']
+
+# Load data
+for sufix, plotops in zip(sufixList, plotoptions):
+	file_M1 = np.loadtxt(FOLDER2SAVE+'sptMF_Mass_'+sufix+'.dat') 
+	file_K1 = np.loadtxt(FOLDER2SAVE+'sptMF_Stiff_'+sufix+'.dat') 
+	file_M2 = np.loadtxt(FOLDER2SAVE+'sptMF_MassDers_'+sufix+'.dat') 
+	file_K2 = np.loadtxt(FOLDER2SAVE+'sptMF_StiffDers_'+sufix+'.dat') 
+
+	degList = file_M1[:, 0]
+	timeElapsedList = [file_M1[:, 1]+file_K1[:, 1], file_M2[:, 1]+file_K2[:, 1]]
+	quadrule = sufix.split('_')[0]
+
+	for i, [timeElapsed, label] in enumerate(zip(timeElapsedList, labels)):
+		color = COLORLIST[i]
+		if quadrule == 'iga':
+			ax.semilogy(degList, timeElapsed, label='IGA-GL '+label, color=color, marker=plotops['marker'],
+						markerfacecolor='w', markersize=plotops['markersize'], linestyle=plotops['linestyle'])
+		else:
+			ax.semilogy(degList, timeElapsed, color=color, marker=plotops['marker'],
+						markerfacecolor='w', markersize=plotops['markersize'], linestyle=plotops['linestyle'])
+
+ax.semilogy([], [], color='k', marker=WQ1Plot['marker'], markerfacecolor='w',
+				markersize=WQ1Plot['markersize'], linestyle=WQ1Plot['linestyle'], label='IGA-WQ 4')
+ax.semilogy([], [], color='k', marker=WQ2Plot['marker'], markerfacecolor='w',
+				markersize=WQ2Plot['markersize'], linestyle=WQ2Plot['linestyle'], label='IGA-WQ 2')
+
+ax.minorticks_off()
+ax.legend(ncol=2, loc='upper center')
+# ax.legend(loc='upper left')
+# ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+ax.set_xlabel('Degree ' + r'$p$')
+ax.set_ylabel('Wall time (s)')
+ax.set_xlim([0, 10])
+ax.set_ylim([1e-1, 1e4])
+fig.tight_layout()
+fig.savefig(FOLDER2SAVE + 'sptMF_time' + '.pdf')
