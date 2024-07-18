c8d37a9b3d88c5528f3fb45528e859d5d14504c5
JoaquinCornejo <joaquin.cornejo-fuentes@insa-lyon.fr>
2024-07-18 17:36:37 +0200

    add elliptic

---
+++ src/iga_wq_mf/thesis/Elliptic/CPU_fastdiag.py
@@ -0,0 +1,77 @@
+from thesis.Elliptic.__init__ import *
+from pysrc.lib.lib_base import createUniformKnotvector_Rmultiplicity
+from pysrc.lib.lib_quadrules import WeightedQuadrature
+
+# Set global variables
+TYPESIMU = 'meca'
+RUNSIMU = False
+degList = np.arange(1, 6)
+cutList = np.arange(5, 9)
+NDIM = 3
+filename = FOLDER2SAVE + 'FD_time'
+
+if RUNSIMU:
+
+	timeMatrix = np.zeros((len(cutList)+1, len(degList)+1))
+	timeMatrix[1:, 0] = np.array([2**i for i in cutList])
+	timeMatrix[0, 1:] = degList
+
+	for i, cuts in enumerate(cutList):
+		for j, degree in enumerate(degList):
+			nbel = 2**cuts
+			knotvector = createUniformKnotvector_Rmultiplicity(degree, nbel)
+			weightedQuad = WeightedQuadrature(degree, knotvector, quadArgs={})
+			info = weightedQuad.getQuadratureRulesInfo()
+			qp, indices, dersbasis, dersweights = info; nbqp = len(qp)
+			nb_ctrlpts = weightedQuad.nbctrlpts
+
+			inpts = [*[nbqp for i in range(NDIM)], *[indices[j] for i in range(NDIM) for j in range(2)], 
+					*[dersbasis for i in range(NDIM)], *[dersweights for i in range(NDIM)]]
+
+			start = time.process_time()
+			if TYPESIMU == 'heat':
+				vector_in = np.random.random(nb_ctrlpts**NDIM)
+				if NDIM == 2: eigensolver.fastdiagonalization_2d(*inpts, vector_in)
+				if NDIM == 3: eigensolver.fastdiagonalization_3d(*inpts, vector_in)
+			elif TYPESIMU == 'meca':
+				vector_in = np.random.random((NDIM, nb_ctrlpts**NDIM))
+				if NDIM == 2: eigensolver.elfastdiagonalization_2d(*inpts, vector_in)
+				if NDIM == 3: eigensolver.elfastdiagonalization_3d(*inpts, vector_in)
+
+			finish = time.process_time()
+			timeElapsed = finish - start
+
+			print('For p = %s, nbel = %s, time: %.4f' %(degree, nbel, timeElapsed))
+			timeMatrix[i+1, j+1] = timeElapsed
+			np.savetxt(filename+TYPESIMU+'.dat', timeMatrix)
+
+# Load data
+fig, ax = plt.subplots(figsize=(6, 4))
+
+file = np.loadtxt(filename+'heat'+'.dat')
+degList = file[0, 1:]; nbelList = file[1:, 0]; timeElapsed = file[1:, 1:]
+for i, degree in enumerate(degList): 
+	ax.loglog(nbelList**3, timeElapsed[:, i], marker='s', color=COLORLIST[0], alpha=(i+1)/len(degList))
+ax.loglog([], [], marker='s', color=COLORLIST[0], label='Steady heat')
+
+file = np.loadtxt(filename+'meca'+'.dat')
+degList = file[0, 1:]; nbelList = file[1:, 0]; timeElapsed = file[1:, 1:]
+for i, degree in enumerate(degList): 
+	ax.loglog(nbelList**3, timeElapsed[:, i], marker='o', color=COLORLIST[1], alpha=(i+1)/len(degList))
+ax.loglog([], [], marker='o', color=COLORLIST[1], label='Elasticity')
+
+ax.xaxis.set_major_formatter(mpl.ticker.ScalarFormatter())
+ax.xaxis.set_minor_formatter(mpl.ticker.NullFormatter())
+
+if NDIM == 2: ...
+if NDIM == 3: 
+	ax.set_xticks([1e4, 32**3, 64**3, 128**3, 256**3, 1e8])
+	ax.set_xticklabels([r'$10^3$', r'$32^3$', r'$64^3$', r'$128^3$', r'$256^3$', r'$10^8$'])
+
+ax.minorticks_off()
+ax.legend(loc='best')
+ax.set_xlabel('Total number of elements')
+ax.set_ylabel('CPU time (s)')
+ax.set_ylim([1e-3, 1e2])
+fig.tight_layout()
+fig.savefig(filename+'.pdf')
---
+++ src/iga_wq_mf/thesis/Elliptic/CPU_matrixfree.py
@@ -0,0 +1,109 @@
+from thesis.Elliptic.__init__ import *
+from pysrc.lib.lib_geomdl import Geomdl
+from pysrc.lib.lib_part import part
+from pysrc.lib.lib_material import heatmat, mechamat
+from pysrc.lib.lib_boundary import boundaryCondition
+from pysrc.lib.lib_job3d import heatproblem, mechaproblem
+
+# Set global variables
+RUNSIMU = True
+degList = range(1, 10)
+cuts = 6
+quadArgs = {'quadrule':'wq', 'type':1}
+
+if RUNSIMU:
+
+	timeMF_conductivity = np.zeros((len(degList), 2))
+	timeMF_conductivity[:, 0] = degList
+
+	timeMF_stiffness = np.zeros((len(degList), 2))
+	timeMF_stiffness[:, 0] = degList
+
+	for i, degList in enumerate(degList):
+		
+		geoArgs = {'name': 'VB', 'degree': degList*np.ones(3, dtype=int), 
+					'nb_refinementByDirection': cuts*np.ones(3, dtype=int)
+		}
+		blockPrint()			
+		modelGeo = Geomdl(geoArgs)
+		modelIGA = modelGeo.getIGAParametrization()
+		modelPhy = part(modelIGA, quadArgs=quadArgs)
+
+		heatmaterial = heatmat()
+		heatmaterial.addCapacity(inpt=1.0, isIsotropic=True)
+		heatmaterial.addConductivity(inpt=1.0, isIsotropic=True, shape=3)
+
+		mecamaterial = mechamat({'elastic_modulus':1e3, 'elastic_limit':1e10, 
+						'poisson_ratio':0.3, 'isoHardLaw': {'name':'none'}})
+
+		# Set Dirichlet boundaries	
+		boundary = boundaryCondition(modelPhy.nbctrlpts)
+		boundary.add_DirichletConstTemperature(table=np.ones((3, 2), dtype=bool))
+		boundary.add_DirichletDisplacement(table=np.ones((3, 2, 3), dtype=bool))
+		enablePrint()
+
+		# Solve elastic problem
+		hproblem = heatproblem(heatmaterial, modelPhy, boundary)
+		mproblem = mechaproblem(mecamaterial, modelPhy, boundary)
+
+		# ------------------
+		# Compute MF product
+		# ------------------
+		enablePrint()
+		print('******')
+		start = time.process_time()
+		hproblem.compute_mfConductivity(np.random.random(boundary._nbctrlpts_total))
+		finish = time.process_time()
+		print('Time Conductivity:%.2e' %(finish-start))
+		timeMF_conductivity[i, 1] = finish - start
+
+		start = time.process_time()
+		mproblem.compute_mfStiffness(np.random.random((3, boundary._nbctrlpts_total)))
+		finish = time.process_time()
+		print('Time Stiffness:%.2e' %(finish-start))
+		timeMF_stiffness[i, 1] = finish - start
+
+		np.savetxt(FOLDER2SAVE+'MF_conductivity_'+quadArgs['quadrule']+'_'+str(quadArgs['type'])+'.dat', timeMF_conductivity)
+		np.savetxt(FOLDER2SAVE+'MF_stiffness_'+quadArgs['quadrule']+'_'+str(quadArgs['type'])+'.dat', timeMF_stiffness)
+
+fig, ax = plt.subplots(figsize=(5, 4))
+IgaPlot = {'marker': 's', 'linestyle': '-', 'markersize': 10}
+WQ1Plot = {'marker': 'x', 'linestyle': '--', 'markersize': 6}
+WQ2Plot = {'marker': 'o', 'linestyle': ':', 'markersize': 6}
+plotoptions = [IgaPlot, WQ1Plot, WQ2Plot]
+sufixList = ['iga_leg', 'wq_1', 'wq_2']
+labels = ['Steady heat', 'Elasticity']
+
+# Load data
+for sufix, plotops in zip(sufixList, plotoptions):
+	file_K1 = np.loadtxt(FOLDER2SAVE+'MF_conductivity_'+sufix+'.dat') 
+	file_S1 = np.loadtxt(FOLDER2SAVE+'MF_stiffness_'+sufix+'.dat') 
+
+	degList = file_K1[:, 0]
+	timeElapsedList = [file_K1[:, 1], file_S1[:, 1]]
+	quadrule = sufix.split('_')[0]
+
+	for i, [timeElapsed, label] in enumerate(zip(timeElapsedList, labels)):
+		color = COLORLIST[i]
+		if quadrule == 'iga':
+			ax.semilogy(degList, timeElapsed, label='IGA-GL '+label, color=color, marker=plotops['marker'],
+						markerfacecolor='w', markersize=plotops['markersize'], linestyle=plotops['linestyle'])
+		else:
+			ax.semilogy(degList, timeElapsed, label='IGA-MF '+label, color=color, marker=plotops['marker'],
+						markerfacecolor='w', markersize=plotops['markersize'], linestyle=plotops['linestyle'])
+
+# ax.semilogy([], [], color='k', marker=WQ1Plot['marker'], markerfacecolor='w',
+# 				markersize=WQ1Plot['markersize'], linestyle=WQ1Plot['linestyle'], label='IGA-MF')
+# ax.semilogy([], [], color='k', marker=WQ2Plot['marker'], markerfacecolor='w',
+# 				markersize=WQ2Plot['markersize'], linestyle=WQ2Plot['linestyle'], label='IGA-WQ 2')
+
+ax.minorticks_off()
+# ax.legend(ncol=2, loc='upper center')
+ax.legend(loc='upper left')
+# ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+ax.set_xlabel('Degree ' + r'$p$')
+ax.set_ylabel('Wall time (s)')
+ax.set_xlim([0, 10])
+ax.set_ylim([1e-1, 2e2])
+fig.tight_layout()
+fig.savefig(FOLDER2SAVE + 'MF_time' + '.pdf')
