d56b01d8c8e4afb5970a6fc44a018c74439942e7
DUVAL Arnaud <arnaud.duval@insa-lyon.fr>
2024-12-06 15:53:38 +0100

    Improve a test.

--- benchs/leastsquare_projection/1_patch/test_leastsquare_projection_1_patch.py
+++ benchs/leastsquare_projection/1_patch/test_leastsquare_projection_1_patch.py
@@ -2,96 +2,101 @@
 
 # This file is part of Yeti.
 #
-# Yeti is free software: you can redistribute it and/or modify it under the terms
-# of the GNU Lesser General Public License as published by the Free Software
-# Foundation, either version 3 of the License, or (at your option) any later version.
+# Yeti is free software: you can redistribute it and/or modify it under the
+# terms of the GNU Lesser General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option)
+#  any later version.
 #
-# Yeti is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
-# without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
-# PURPOSE. See the GNU Lesser General Public License for more details.
+# Yeti is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+# details.
 #
-# You should have received a copy of the GNU Lesser General Public License along
-# with Yeti. If not, see <https://www.gnu.org/licenses/>
+# You should have received a copy of the GNU Lesser General Public License
+# along with Yeti. If not, see <https://www.gnu.org/licenses/>
 
-# Single domain with 1 patch
 
-# Python module
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+A single domain with 1 patch
+Test generation of a vtu file with with computation of values at nodes points
+using least square projection of values at Gauss points.
+"""
+
+
+import os
+
 import numpy as np
 import scipy.sparse as sp
-import sys
-import time
 
-#IGA module
+# pylint: disable=c-extension-no-member
+# pylint: disable=no-name-in-module
 from yeti_iga.preprocessing.igaparametrization import IGAparametrization
-from yeti_iga.stiffmtrx_elemstorage import sys_linmat_lindef_static as build_stiffmatrix
+from yeti_iga.stiffmtrx_elemstorage import sys_linmat_lindef_static \
+    as build_stiffmatrix
 import yeti_iga.reconstructionSOL as rsol
 import yeti_iga.postprocessing.postproc as pp
-from yeti_iga.preprocessing.igaparametrization import IGAmanip as manip
-
-FILENAME = 'plateWithRoundHole'
-
-# Creation of the IGA object
-# --------------------------
-modeleIGA = IGAparametrization(filename=FILENAME)
-
-nb_deg = np.zeros((3,modeleIGA._nb_patch),dtype=np.intp)
-nb_ref = np.zeros((3,modeleIGA._nb_patch),dtype=np.intp)
-additional_knots = {"patches":np.array([]),"1":np.array([]),"2":np.array([]),"3":np.array([])}
-
-nb_deg[:,0] = [0,0,0]
-nb_ref[:,0] = [3,3,0]
-
-modeleIGA.refine(nb_ref,nb_deg,additional_knots)
 
 
-# --
-# STATIC STUDY
+def test_leastsquare_projection_1_patch():
+    """
+    Compute solution and generate VTU file
+    """
+    # TODO: improve write location of output files
 
-# MATRIX Assembly
+    # Read data and create IGAparametrization object
+    script_dir = os.path.dirname(os.path.realpath(__file__))
+    iga_model = IGAparametrization(filename=f'{script_dir}/plateWithRoundHole')
 
-ndof = modeleIGA._nb_dof_free
-idof = modeleIGA._ind_dof_free[:ndof]-1
+    # Model refinement
+    iga_model.refine(np.array([3, 3, 0]).transpose(),
+                     np.array([0, 0, 0]).transpose())
 
-t1 = time.time()
-data,row,col,Fb = build_stiffmatrix( *modeleIGA.get_inputs4system_elemStorage() )
-Kside = sp.coo_matrix((data,(row,col)), shape=(modeleIGA._nb_dof_tot,modeleIGA._nb_dof_tot),
-                      dtype='float64').tocsc()
-Ktot  = Kside + Kside.transpose()
-K2solve = Ktot[idof,:][:,idof]
-del Kside,data,row,col
-print(('\n Time to build stiffness matrix : %.2f s' % (time.time() - t1)))
+    # Matrix Assembly
+    ndof = iga_model.nb_dof_free
+    idof = iga_model.ind_dof_free[:ndof]-1
+
+    data, row, col, rhs = build_stiffmatrix(
+        *iga_model.get_inputs4system_elemStorage())
+    stiff_side = sp.coo_matrix(
+        (data, (row, col)),
+        shape=(iga_model.nb_dof_tot, iga_model.nb_dof_tot),
+        dtype='float64').tocsc()
+    stiff_tot = stiff_side + stiff_side.transpose()
+
+    # Resolution
+    x = sp.linalg.spsolve(stiff_tot[idof, :][:, idof], rhs[idof])
+
+    # Standard postprocessing
+    sol, _ = rsol.reconstruction(**iga_model.get_inputs4solution(x))
+    # Stress and strain post processing is deactivated due to a singular point
+    # leading to NaN values
+    pp.generatevtu(*iga_model.get_inputs4postprocVTU(
+        'linear_analysis', sol.transpose(), nb_ref=np.array([0, 0, 0]),
+        Flag=np.array([True, False, False])))
+
+    # Postprocessing with least square projection
+    # Compute gram matrix
+    data, row, col = pp.build_cgrammatrix(*iga_model.get_inputs4grammat())
+    # RHS vector
+    rhs = pp.compute_svars_solid_rhs(
+        *iga_model.get_inputs4svarsrhs(sol.transpose()))
+
+    gram_side = sp.coo_matrix((data, (row, col)),
+                              shape=(max(col)+1,
+                              max(col) + 1),
+                              dtype='float64').tocsc()
+    gram_tot = gram_side + gram_side.transpose()
+
+    projected_svars = sp.linalg.spsolve(gram_tot, rhs.transpose())
+
+    pp.generate_proj_vtu(*iga_model.get_inputs4proj_vtu('least_square',
+                         sol.transpose(),
+                         projected_svars.transpose(),
+                         nb_ref=3*np.array([1, 1, 0])))
 
 
-# SOLVING : direct solver
-t2 = time.time()
-LU = sp.linalg.splu(K2solve)
-x  = LU.solve(Fb[idof])
-print(('\n Time for direct solving :    %.2f s\n\n' % (time.time() - t2)))
-
-# Standard postprocessing
-# -----------------------
-SOL,u = rsol.reconstruction(**modeleIGA.get_inputs4solution(x))
-# Stress and strain post processing is deactivated due to a singular point
-# leading to NaN values
-pp.generatevtu(*modeleIGA.get_inputs4postprocVTU(
-    'linear_analysis',SOL.transpose(),nb_ref=3*np.array([0,0,0]),
-    Flag=np.array([True,False,False])))
-
-# Postprocessing with least square projection
-# -------------------------------------------
-# Compute gram matrix
-data, row, col = pp.build_cgrammatrix( *modeleIGA.get_inputs4grammat())
-# RHS vector
-rhs = pp.compute_svars_solid_rhs( *modeleIGA.get_inputs4svarsrhs(SOL.transpose()))
-
-size = max(col)+1
-Gside = sp.coo_matrix((data,(row,col)), shape=(size,size),
-                      dtype='float64').tocsc()
-Gtot  = Gside + Gside.transpose()
-
-del Gside,data,row,col
-LU = sp.linalg.splu(Gtot)
-
-projected_svars = LU.solve(rhs.transpose())
-
-pp.generate_proj_vtu(*modeleIGA.get_inputs4proj_vtu('least_square', SOL.transpose(), projected_svars.transpose(), nb_ref=3*np.array([1,1,0])))
+if __name__ == '__main__':
+    test_leastsquare_projection_1_patch()
