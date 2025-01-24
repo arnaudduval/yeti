b2e32868740b6d65505ec325aae0d90e22e2e06b
DUVAL Arnaud <arnaud.duval@insa-lyon.fr>
2025-01-24 17:03:24 +0100

    Debug a test not working in CI

--- src/yeti_iga/postprocessing/generate_coupling_vtu.f90
+++ src/yeti_iga/postprocessing/generate_coupling_vtu.f90
@@ -226,10 +226,10 @@ subroutine generate_coupling_vtu(filename, output_path,     &
                 do i_vertice = 1, nb_vertice
                     call updateElementNumber(xi(i_side,:, i_vertice, i_elem))
                     sctr(:nnode_patch) = ien_patch(:, current_elem)
-            !         do i = 1, nnode_patch
-            !             coords_elem(:,i) = coords3D(:mcrd, sctr(i))
-            !             u_elem(:,i) = sol(:mcrd, sctr(i))
-            !         enddo
+                    do i = 1, nnode_patch
+                        coords_elem(:,i) = coords3D(:mcrd, sctr(i))
+                        u_elem(:,i) = sol(:mcrd, sctr(i))
+                    enddo
             !         !! Evaluate basis function
             !         call evalnurbs(xi(i_side,:, i_vertice, i_elem), R(:nnode_patch), dRdxi(:nnode_patch,:))
 
