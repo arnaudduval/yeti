25623c297e0d5f957af210a7c63857d07fdc0af0
DUVAL Arnaud <arnaud.duval@insa-lyon.fr>
2025-04-24 16:39:55 +0200

    Add a __version__ property to the package

--- src/yeti_iga/__init__.py
+++ src/yeti_iga/__init__.py
@@ -5,3 +5,10 @@ from .overlay.boundary_condition import BoundaryCondition
 from .overlay.distributed_load import DistributedLoad
 from .overlay.iga_optimization import IgaOptimization
 from .overlay.refinement import Refinement
+
+try:
+    from importlib.metadata import version
+except ImportError:
+    from importlib_metadata import version  # compatibilty < Python 3.8
+
+__version__ = version("yeti_iga")
