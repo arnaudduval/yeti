18af60cc4485039c454cc494be553d584afdcb64
DUVAL Arnaud <arnaud.duval@insa-lyon.fr>
2025-05-27 15:21:40 +0200

    Update Tbeam example

--- benchs/Tbeam2cplg/mono_analysis.py
+++ benchs/Tbeam2cplg/mono_analysis.py
@@ -2,15 +2,15 @@
 
 # This file is part of Yeti.
 #
-# Yeti is free software: you can redistribute it and/or modify it under the terms 
-# of the GNU Lesser General Public License as published by the Free Software 
+# Yeti is free software: you can redistribute it and/or modify it under the terms
+# of the GNU Lesser General Public License as published by the Free Software
 # Foundation, either version 3 of the License, or (at your option) any later version.
 #
-# Yeti is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
-# without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+# Yeti is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+# without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
 # PURPOSE. See the GNU Lesser General Public License for more details.
 #
-# You should have received a copy of the GNU Lesser General Public License along 
+# You should have received a copy of the GNU Lesser General Public License along
 # with Yeti. If not, see <https://www.gnu.org/licenses/>
 
 #!/usr/bin/env python
@@ -35,12 +35,12 @@ import sys
 import time
 
 #IGA module
-from preprocessing.igaparametrization import IGAparametrization
-from stiffmtrx_elemstorage import sys_linmat_lindef_static as build_stiffmatrix
-from coupling.cplgmatrix import cplg_matrix
-import reconstructionSOL as rsol
-import postprocessing.postproc as pp
-from preprocessing.igaparametrization import IGAmanip as manip
+from yeti_iga.preprocessing.igaparametrization import IGAparametrization
+from yeti_iga.stiffmtrx_elemstorage import sys_linmat_lindef_static as build_stiffmatrix
+from yeti_iga.coupling.cplgmatrix import cplg_matrix
+import yeti_iga.reconstructionSOL as rsol
+import yeti_iga.postprocessing.postproc as pp
+from yeti_iga.preprocessing.igaparametrization import IGAmanip as manip
 
 EXEMPLE_NB = 5
 
@@ -59,7 +59,7 @@ nb_deg = np.zeros((3,modeleIGA._nb_patch),dtype=np.intp)
 nb_ref = np.zeros((3,modeleIGA._nb_patch),dtype=np.intp)
 additional_knots = {"patches":np.array([]),"1":np.array([]),"2":np.array([]),"3":np.array([])}
 
-# Data read from input file are 
+# Data read from input file are
 # - two shells of degree 1
 # - 4 interface elements of degree 1
 # - 2 Lagrange fields of degree 0
@@ -73,7 +73,7 @@ nb_ref[:,0]  = np.array([3+r,2+r,0])
 additional_knots["patches"] = np.array([1])
 additional_knots["1"] = np.linspace(0,1,10*2**r)[1:-1]
 additional_knots["2"] = np.linspace(0,1,4*2**r)[1:-1]
-    
+
 nb_ref[0,2:6]= 5+r
 
 nb_deg[0,6:] = np.array([p,p-1])
@@ -88,9 +88,9 @@ modeleIGA.refine(nb_ref,nb_deg,additional_knots)
 modeleIGA._NBPINT[ np.where(modeleIGA._ELT_TYPE == 'U00') ] = 6**modeleIGA._dim.min()
 
 # Add boundary conditions : lock displacement for the 2nd row of CPs
-icps0 = manip.get_boundCPindice_wEdges(modeleIGA._Nkv,modeleIGA._Jpqr,modeleIGA._dim, 1, 
+icps0 = manip.get_boundCPindice_wEdges(modeleIGA._Nkv,modeleIGA._Jpqr,modeleIGA._dim, 1,
                                            num_patch=0, offset=1)
-icps1 = manip.get_boundCPindice_wEdges(modeleIGA._Nkv,modeleIGA._Jpqr,modeleIGA._dim, 1, 
+icps1 = manip.get_boundCPindice_wEdges(modeleIGA._Nkv,modeleIGA._Jpqr,modeleIGA._dim, 1,
                                            num_patch=1, offset=1)
 for i in np.arange(0,3):
     manip.add_displacementBC(modeleIGA, modeleIGA._indCPbyPatch[0][icps0], i+1, 0.)
@@ -107,7 +107,7 @@ idof = modeleIGA._ind_dof_free[:ndof]-1
 
 t1 = time.time()
 data,row,col,Fb = build_stiffmatrix( *modeleIGA.get_inputs4system_elemStorage() )
-Kside = sp.coo_matrix((data,(row,col)), shape=(modeleIGA._nb_dof_tot,modeleIGA._nb_dof_tot), 
+Kside = sp.coo_matrix((data,(row,col)), shape=(modeleIGA._nb_dof_tot,modeleIGA._nb_dof_tot),
                       dtype='float64').tocsc()
 Ktot  = Kside + Kside.transpose()
 del Kside,data,row,col
@@ -148,17 +148,17 @@ print(modeleIGA._mcrd)
 print(modeleIGA._nb_cp)
 
 
-# compute angle 
+# compute angle
 from postprocessing.postproc import displayangledeformconfig
-    
+
 displayangledeformconfig(*modeleIGA.get_inputs4postprocCPLG('test',SOL.transpose(),nb_ref=10))
-    
+
 data = np.loadtxt('results/test.txt',delimiter=',')
 n  = int(data.shape[0]/2)
 print(n)
 v1 = data[:n,1:]
 v2 = data[n:,1:]
-    
+
 deg = np.rad2deg(np.arccos(np.sum(v1*v2,axis=1)))
 err = np.abs(deg-90.)/90.
 newdata = np.array([data[:n,0],deg,err]).transpose()
@@ -211,13 +211,13 @@ if False:
         SOL3D = np.c_[SOL,np.zeros(SOL.shape[0])]
     else:
         SOL3D = SOL
-        
+
     from optim.gradcompliance import gradcomp_an, gradcplg_an
-    dComp_fine = gradcomp_an( 
+    dComp_fine = gradcomp_an(
         *modeleIGA.get_inputs4gradCompliance( SOL3D.T ) )
-    dComp_fine+= gradcplg_an( 
+    dComp_fine+= gradcplg_an(
         *modeleIGA.get_inputs4gradCoupling( SOL3D.T ) )
-    
+
     modeleIGA.generate_vtk4controlMeshVisu(
         'sensitivityAnalysis',0,sol=dComp_fine )
 
@@ -240,11 +240,11 @@ if False:
     # - dof infos
     ndof = modeleIGA._nb_dof_free
     idof = modeleIGA._ind_dof_free[:ndof]-1
-    
+
     list_patch = np.where(np.isin(modeleIGA._ELT_TYPE,np.array(['U1','U3','U30'])))[0] + 1
     list_curve = np.where(np.isin(modeleIGA._ELT_TYPE,np.array(['U00'])))[0] + 1
     list_lgrge = np.where(np.isin(modeleIGA._ELT_TYPE,np.array([ 'U4'])))[0] + 1
-    
+
     idof_internal = np.array([],dtype=np.intp)
     mcrd = modeleIGA._mcrd
     for p in list_patch-1:
@@ -252,7 +252,7 @@ if False:
             (idof_internal,np.repeat(np.unique(modeleIGA._IEN[p])-1,mcrd)*mcrd
              + np.tile(np.arange(0,mcrd),modeleIGA._indCPbyPatch[p].size)))
     idof_internal = np.intersect1d(idof_internal,idof)
-    
+
     idof_lgrge = np.array([],dtype=np.intp)
     idof_lgrge_list = []
     for p in list_lgrge-1:
@@ -260,20 +260,20 @@ if False:
                + np.tile(np.arange(0,mcrd),modeleIGA._indCPbyPatch[p].size)
         idof_lgrge = np.concatenate((idof_lgrge,temp))
         idof_lgrge_list.append(np.intersect1d(temp,idof))
-    
+
     idof_lgrge_tot = np.intersect1d(idof_lgrge,idof)
     idof_lgrge     = idof_lgrge_list
-    
+
     # - operators
     K2solve = Ktot[idof_internal,:][:,idof_internal]
     f2solve =   Fb[idof_internal]
-    
+
     C2solve = Ctot[idof_lgrge_tot,:][:,idof_internal]
 
     # - factorization
     from solver import pseudoLUstep
     LU = pseudoLUstep(K2solve,tol=1.e-8)
-    
+
     def shur(x):
         y = np.zeros_like(x)
         y[:] = C2solve * LU.solve(C2solve.T * x)
@@ -281,7 +281,7 @@ if False:
     dofl = C2solve.shape[0]
     Sd = sp.linalg.LinearOperator((dofl,dofl),matvec=shur)
     t  = C2solve*LU.solve(f2solve)
-    
+
     # - projector
     R = LU.R
     G =-C2solve*R
@@ -294,11 +294,11 @@ if False:
         y[:] = v[:] - G * invGG.dot( G.T*v )
         return y
     Proj = sp.linalg.LinearOperator((dofl,dofl),matvec=project)
-    
+
     lmbda0 = G.dot(invGG.dot(e))
 
     # - preconditionner
-    
+
     # - resolution
     from solver import PCPGortho
     t1 = time.time()
@@ -306,13 +306,13 @@ if False:
                             M=None, tol=1.e-8, maxiter=300,savetxt=True)
     print(' Resolution with PCPG algo')
     print((' (duration : %.2f s, nb iter : %i).\n\n' % (time.time() - t1,nbiter)))
-    
-    
+
+
     # - postproc
     alpha = invGG.dot( G.T * (t - Sd(lmbda) ) )
     utot = np.zeros(modeleIGA._nb_dof_tot)
     utot[idof_internal] = LU.solve(f2solve - C2solve.T*lmbda) + R*alpha
-    
+
     SOL,u = rsol.reconstruction(*modeleIGA.get_inputs4solution(utot[idof]))
     pp.generatevtu(*modeleIGA.get_inputs4postprocVTU(
         'coupling_pcpg',SOL.transpose(),nb_ref=np.array([4,4,1]),
@@ -321,21 +321,21 @@ if False:
 
 
 
-## compute angle 
+## compute angle
 #from postprocessing.postproc_cplg import displayangledeformconfig
-    
+
 #displayangledeformconfig(*modeleIGA.get_inputs4postprocCPLG('test',SOL.transpose(),nb_ref=10))
-    
+
 #data = np.loadtxt('results/test.txt',delimiter=',')
 #n  = data.shape[0]/2
 #v1 = data[:n,1:]
 #v2 = data[n:,1:]
-    
+
 #deg = np.rad2deg(np.arccos(np.sum(v1*v2,axis=1)))
 #err = np.abs(deg-90.)/90.
 #newdata = np.array([data[:n,0],deg,err]).transpose()
 #np.savetxt('temp/rotTbeam%i.txt'%r,newdata,delimiter=',')
-    
+
 '''
 
 
@@ -347,7 +347,7 @@ ipl = np.array([7,8])
 
 dof1 = np.array([],dtype=np.intp)
 for p in ip1:
-    dof1 = np.concatenate((dof1,np.repeat(np.unique(modeleIGA._IEN[p])-1,3)*3 
+    dof1 = np.concatenate((dof1,np.repeat(np.unique(modeleIGA._IEN[p])-1,3)*3
                            + np.tile(np.array([0,1,2]),modeleIGA._indCPbyPatch[p].size)))
 dof1 = np.intersect1d(dof1,idof)
 
@@ -357,7 +357,7 @@ LU1 = sp.linalg.splu(K1)
 
 dof2 = np.array([],dtype=np.intp)
 for p in ip2:
-    dof2 = np.concatenate((dof2,np.repeat(np.unique(modeleIGA._IEN[p])-1,3)*3 
+    dof2 = np.concatenate((dof2,np.repeat(np.unique(modeleIGA._IEN[p])-1,3)*3
                            + np.tile(np.array([0,1,2]),modeleIGA._indCPbyPatch[p].size)))
 #dof2 = np.intersect1d(dof2,idof)
 K2   = Ktot[dof2,:][:,dof2]
@@ -389,7 +389,7 @@ F2  = Fb[dof2]
 
 dofl = np.array([],dtype=np.intp)
 for p in ipl:
-    dofl = np.concatenate((dofl,np.repeat(np.unique(modeleIGA._IEN[p])-1,3)*3 
+    dofl = np.concatenate((dofl,np.repeat(np.unique(modeleIGA._IEN[p])-1,3)*3
                            + np.tile(np.array([0,1,2]),modeleIGA._indCPbyPatch[p].size)))
 dofl = np.intersect1d(dofl,idof)
 
@@ -407,7 +407,7 @@ def shurOp_wLU(di):
     al = di[-6:]
     y1 = LU1.solve(C1.transpose().dot(ld))
     y2 = K2star(C2.transpose().dot(ld))
-    
+
     ytot = np.zeros_like(di)
     ytot[:-6] = C1.dot(y1) + C2.dot(y2) -C2.dot(R2.dot(al))
     ytot[-6:] = -(C2.dot(R2).transpose()).dot(ld)
