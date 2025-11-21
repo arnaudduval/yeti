4b7fa64e69e9f2c997e48bce131db2e65ab332ca
DUVAL Arnaud <arnaud.duval@insa-lyon.fr>
2025-11-21 17:37:58 +0100

    Add patch evaluation for multiple points (not optimized, 2D only)

--- src/yeti_iga/future/include/Patch.hpp
+++ src/yeti_iga/future/include/Patch.hpp
@@ -2,6 +2,8 @@
 #include <vector>
 #include <pybind11/numpy.h>
 #include "BSpline.hpp"
+#include "BSplineTensor.hpp"
+#include "ControlPointManager.hpp"
 
 namespace py = pybind11;
 
@@ -23,34 +25,15 @@ struct Patch {
         : tensor(t), global_indices(mapping), local_shape(local_shape_), cp_manager(mgr) {}
 
     // get view (zero copy) to coordinates of local control point i_local
-    double* local_cp_ptr(size_t i_local) {
-        size_t gid = global_indices.at(i_local);
-        return cp_manager->coords.data() + gid * cp_manager->dim_phys;
-    }
+    double* local_cp_ptr(size_t i_local);
 
     // Numpy 1D view on a local point
-    py::array_t<double> local_cp_view(size_t i_local) {
-        double* ptr = local_cp_ptr(i_local);
-        auto capsule = py::capsule(ptr); // do not deallocate
-        return py::array_t<double>(
-            {cp_manager->dim_phys},       // shape 1D
-            {sizeof(double)},             // stride
-            ptr,
-            capsule
-        );
-    }
+    py::array_t<double> local_cp_view(size_t i_local);
 
     // get a numpy view for all local control points
     // shape = local_shape + (dim_phys,)
-    py::array_t<double> local_control_point_view() const {
-        std::vector<ssize_t> shape;
-        for (auto s : local_shape) shape.push_back((ssize_t)s);
-        shape.push_back((ssize_t)cp_manager->dim_phys);
+    py::array_t<double> local_control_point_view() const;
 
-        // return a view on the *global* coords with shape (n_points_global, dim_phys).
-        std::vector<ssize_t> gshape = {(ssize_t)cp_manager->n_points(), (ssize_t)cp_manager->dim_phys};
-        auto arr = py::array_t<double>(gshape, { (ssize_t)sizeof(double)*cp_manager->dim_phys, (ssize_t)sizeof(double) }, cp_manager->coords.data());
-        // User can index arr[global_indices] in Python to build local array (zero-copy slicing not trivial).
-        return arr;
-    }
+    py::array_t<double> EvaluatePatchND(const py::array_t<int> spans,
+                                        const py::array_t<double>& u) const;
 };
