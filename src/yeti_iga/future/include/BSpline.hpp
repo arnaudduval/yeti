f1791eae7edab6633cb034ad0d3b6eb32526f2b5
DUVAL Arnaud <arnaud.duval@insa-lyon.fr>
2025-12-05 16:08:10 +0100

    Add computation of BSpline functions derivatives

--- src/yeti_iga/future/include/BSpline.hpp
+++ src/yeti_iga/future/include/BSpline.hpp
@@ -12,6 +12,8 @@ public:
     int FindSpan(double u) const;
     py::array_t<double> BasisFuns(int span, double u) const;
     void BasisFuns_raw(int span, double u, double* out) const;
+    // TODO plutôt appeler cette fonction ..._raw ?
+    void BasisFunsDerivatives(int span, double u, int n, double* ders_out) const;
     double OneBasisFun(double u, int i);
     int getDegree() const {return degree; }
     const std::vector<double>& getKnotVector() const { return kvector; }
@@ -21,3 +23,6 @@ private:
     int degree;
     std::vector<double> kvector;
 };
+
+// Interface for python binding
+py::array_t<double> bspline_basis_funs_derivatives(const BSpline& self, int span, double u, int d);
--- src/yeti_iga/future/src/bspline/BSpline.cpp
+++ src/yeti_iga/future/src/bspline/BSpline.cpp
@@ -80,6 +80,93 @@ void BSpline::BasisFuns_raw(int span, double u, double* out) const {
     }
 }
 
+// Compute derivatives of basis functions
+// cnvention : ders[k][i] -> k = 0..n, i = 0..p
+// indexation : buffer[k*(p+1) + i]
+// Algo Piegl & Tiller
+void BSpline::BasisFunsDerivatives(int span, double u, int n, double* ders_out) const {
+    const int p = degree;
+    const std::vector<double>& U = kvector;
+
+    if (n > p) n = p;
+
+    // Offsets
+    const int stride = p + 1;
+
+    std::vector<double> ndu((p+1)*(p+1));
+    std::vector<double> left(p+1), right(p+1);
+
+    ndu[0] = 1.0;
+
+    for (int j = 1; j <= p; ++j) {
+        left[j] = u - U[span + 1 - j];
+        right[j] = U[span + j] - u;
+
+        double saved = 0.;
+        for (int r = 0; r < j; ++r) {
+            ndu[j*stride + r] = right[r+1] + left[j-r];
+            double temp = ndu[r*stride + (j-1)] / ndu[j*stride + r];
+
+            ndu[r*stride + j] = saved + right[r+1] * temp;
+            saved = left[j-r] * temp;
+        }
+        ndu[j*stride + j] = saved;
+    }
+
+    // Load basis values
+    for (int j = 0; j <= p; ++j)
+        ders_out[j] = ndu[j*stride + p];
+
+    // Compute derivatives
+    std::vector<double> a(2*(p+1));
+    for (int r = 0; r <= p; ++r) {
+        int s1=0;
+        int s2=1;
+        a[0] = 1.;
+
+        // Derivatives order k = 1..d
+        for (int k = 1; k <= n; ++k) {
+            double d = 0.;
+            int rk = r - k;
+            int pk = p - k;
+            int j1, j2;
+
+            if (r >= k) {
+                a[s2*stride] = a[s1*stride] / ndu[(pk+1)*stride + rk];
+                d = a[s2*stride] * ndu[rk*stride + pk];
+            }
+            if (rk >= -1)
+                j1 = 1;
+            else
+                j1 = -rk;
+            if (r-1 <= pk)
+                j2 = k-1;
+            else
+                j2 = p-r;
+
+            for (int j = j1; j <= j2; ++j) {
+                a[s2*stride+j] = (a[s1*stride+j] - a[s1*stride+j-1])/ndu[(pk+1)*stride+rk+j];
+                d += a[s2*stride+j]*ndu[(rk+j)*stride + pk];
+            }
+
+            if (r <= pk) {
+                a[s2*stride+k] = -a[s1*stride+k-1]/ndu[(pk+1)*stride+r];
+                d += a[s2*stride + k] * ndu[r*stride+pk];
+            }
+            ders_out[k*stride+r] = d;
+            int j = s1; s1=s2; s2=j;
+
+        }
+    }
+
+    int r = p;
+    for (int k = 1; k <= n; ++k)
+    {
+        for (int j = 0; j <= p; ++j)
+            ders_out[k*stride + j] *= r;
+        r *= (p-k);
+    }
+}
 
 
 double BSpline::OneBasisFun(double u, int i) {
@@ -121,4 +208,21 @@ double BSpline::OneBasisFun(double u, int i) {
 
     }
     return N[0];
+}
+
+// Interface for Python binding
+py::array_t<double> bspline_basis_funs_derivatives(const BSpline& self, int span, double u, int d) {
+    const int p = self.getDegree();
+    if (d > p) d = p;
+
+    const ssize_t rows = d + 1;
+    const ssize_t cols = p + 1;
+
+    // output array
+    py::array_t<double> arr({rows, cols});
+    double* ptr = arr.mutable_data();
+
+    self.BasisFunsDerivatives(span, u, d, ptr);
+
+    return arr;
 }
